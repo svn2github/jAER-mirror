ab5b247245066f1372e6b4f3041f0f9915c90e6f
tobidelbruck <tobidelbruck@b7f4320f-462c-0410-a916-d9f35bb82d52>
2007-06-03 13:48:55 +0000

    updated with sf logo. removed unused images.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@75 b7f4320f-462c-0410-a916-d9f35bb82d52

--- web/index.php
+++ web/index.php
@@ -32,11 +32,13 @@
       CONTENT="5; URL=http://jaer.wiki.sourceforge.net">
 <BODY>
  <!-- SiteSearch Google -->
-<h1>jAER - Java AER</h1>
+<a href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=181834&amp;type=5" width="210" height="62" border="0" alt="SourceForge.net Logo" /></a>
+
+<h1>jAER - Java tools for Address Event Representation (AER)</h1>
 <h3>The home page is the jAER project is now at <a href="http://jaer.wiki.sourceforge.net">http://jaer.wiki.sourceforge.net/</a></h3>
 <p>You are being redirected there.<br>
   <em>
-  <!-- #BeginDate format:Am1 -->May 7, 2007<!-- #EndDate -->
+  <!-- #BeginDate format:Am1 -->June 3, 2007<!-- #EndDate -->
 </em></p>
 </BODY>
 </HTML>
