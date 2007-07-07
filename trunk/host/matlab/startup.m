1bb8287b2670c6b1cb692135b9ef45fd8830283f
tobidelbruck <tobidelbruck@b7f4320f-462c-0410-a916-d9f35bb82d52>
2007-07-07 16:28:30 +0000

    RotateFilter.java: imrpoved performance by caching local vars for chip size. FilterPanel.java: now correctly builds sub panels for enclosed FilterChains FilterChain.java: now has enclosed flag and can have an enclosing filter. (FilterChain needs to expose interface like EventFilter for full generality). HoughLineTracker.java: added tooltips for properties. Called resetFilter on chip update() to reset values correctly. MultiLineClusterTracker.java: removed enclosed filterchain and moved it to new inner class in Driver. Same for HoughLineTracker.java. Driver.java: built enclosed DriverPreFilter chain for common preprocessing. Made prefrences work correctly. matlab/startup.m: fixed paths and removed factory building of CypressFX2Factory.java. Still won't find classes from matlab. FrameAnnotater.java: is mostly broken because filters should annotate only if enclosing filter has run and filter is enabled. Also, annotators are separate from FilterChain which means that when filter is removed, annotation remains, leading to memory leaks.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@117 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/host/matlab/startup.m
+++ trunk/host/matlab/startup.m
@@ -2,13 +2,14 @@
 % on startup setup the java class path for the stuff here
 
 % sets up path to use usb2 java classes
-disp 'setting up java classpath for usb ae/bias interfacing'
+disp 'setting up java classpath for jAER interfacing'
 here=fileparts(mfilename('fullpath'));
 % jars are up and down from us
 p=[here,'\..\java\'];
-javaaddpath([p,'dist\usb2aemon.jar']);
-javaaddpath([p,'jars\usbiojava.jar']);
+javaaddpath([p,'jars\swing-layout-0.9.jar']);
+javaaddpath([p,'jars\UsbIoJava.jar']);
+javaaddpath([p,'dist\jAER.jar']);
 %javaaddpath([p,'jars\jogl.jar']);  % if you get complaint here, remove the jogl in matlab's static classpath.txt
 
-global factory
-factory = ch.unizh.ini.caviar.hardwareinterface.usb.CypressFX2Factory.instance()
+% global factory
+% factory = ch.unizh.ini.caviar.hardwareinterface.usb.CypressFX2Factory.instance()
