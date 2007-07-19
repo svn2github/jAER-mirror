8f98243aff93272b4b5698cc5c92153ee915255b
acardona <acardona@b7f4320f-462c-0410-a916-d9f35bb82d52>
2007-07-19 22:46:24 +0000

    Added the Monster Truck Simulator, as a Blender file. Read the README for a compilation details and the user manual.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@231 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ trunk/blender/camera_event_generator.c
@@ -0,0 +1,137 @@
+/* Copyright Albert Cardona 2007 
+ *
+ * Telluride 2007
+ *
+ */
+#include <Python.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+/* Gets a list of integers that represent an image, and scales them to max 128 in width and height, preserving aspect ratio.
+ * Uses crude nearest point.
+ */
+PyObject* scaleImage(PyObject* self, PyObject* args) {
+	PyObject* pix_ob;
+	PyObject* width_ob;
+	if (!PyArg_ParseTuple(args, "O!O!",
+				&PyList_Type, &pix_ob,
+				&PyInt_Type, &width_ob)) {
+		printf("\nscaleImage arguments are not proper.\n");
+		Py_INCREF(Py_None);
+		return Py_None;
+	}
+	const int width = (int) PyInt_AsLong(width_ob);
+	const int length = PyList_GET_SIZE(pix_ob);
+	const int height = length / width;
+	int w, h;
+	if (width > 128) {
+		w = 128;
+		h = (int)((128.0 / width) * height);
+		if (h > 128) {
+			h = 128;
+			w = (int)((128.0 / height) * 128);
+		}
+	} else if (height > 128) {
+		h = 128;
+		w = (int)((128.0 / height) * width);
+	} else {
+		// untouched
+		return pix_ob;
+	}
+	//printf("New dimensions: %i , %i", w, h);
+	// new array, using crude nearest-point
+	const int length2 = w * h;
+	PyObject* pix_ob2 = PyList_New(length2);
+	const double sx = ((double)w) / width;
+	const double sy = ((double)h) / height;
+	int i;
+	for (i=0; i<length2; i++) {
+		const int x1 = (int)((i % w) / sx);
+		const int y1 = (int)((i / w) / sy);
+		PyList_SET_ITEM(pix_ob2, i, PyInt_FromLong(PyInt_AsLong(PyList_GET_ITEM(pix_ob, y1 * width + x1)))); // duplicating the number wrapper
+		//PyList_SET_ITEM(pix_ob2, i, PyList_GET_ITEM(pix_ob, y1 * width + x1)); // SEG FAULT! without duplicating the number wrapper
+	}
+	PyObject* list = PyList_New(2);
+	PyList_SET_ITEM(list, 0, pix_ob2);
+	PyList_SET_ITEM(list, 1, PyInt_FromLong(w));
+	// return both the new image and the new width
+	return list;
+}
+
+/* Takes to byte arrays and the width, and creates the events as x,y,log(pixdiff) 
+ * Returns a list of integers, each with a packed event (will need to be casted to 16-bit before sending to the jAER)
+ */
+PyObject* createEvents(PyObject* self, PyObject* args) {
+	int i;
+	// obtain arguments
+	PyObject* matrix_ob;
+	PyObject* pix_ob;
+	PyObject* width_ob;
+	PyObject* threshold_ob;
+	if (!PyArg_ParseTuple(args, "O!O!O!O!",
+				&PyList_Type, &matrix_ob,
+				&PyList_Type, &pix_ob,
+				&PyInt_Type, &width_ob,
+				&PyFloat_Type, &threshold_ob)) {
+		printf("\ncreateEvents arguments are not proper.\n");
+		Py_INCREF(Py_None);
+		return Py_None;
+	}
+	// generate events from pixels that have changed
+	const int length = PyList_GET_SIZE(pix_ob); // both matrix and pix have the same size
+	const int width = (int) PyInt_AsLong(width_ob);
+	//const int height = length / width;
+	const double threshold = PyFloat_AsDouble(threshold_ob);
+	int a, b;
+	double val;
+	int* event = malloc(sizeof(int) * length);
+	int count = 0;
+	for (i=0; i<length; i++) {
+		a = (int) PyInt_AsLong(PyList_GET_ITEM(matrix_ob, i));
+		b = (int) PyInt_AsLong(PyList_GET_ITEM(pix_ob, i));
+		/*if (i < 10) {
+			printf("a, b: %i , %i\n", a , b);
+		}*/
+		if (a == b) continue;
+		val = log(a) - log(b);
+		// if any changes:
+		if (fabs(val) > threshold) {
+			// generate event: jAER event is a 16 bit, with first bit zero, 7 bits for y, 7 bits for x and final bit for positive/negative change. The code below will NOT work as expected if the X and Y 's meaningful part is larger than 7-bit
+			event[count] =  /* X */ ((width - (i % width))<<1) /* X needs to be shifted */
+				      + /* Y */ ((i / width)<<8)
+				      + /* SIGN */ (val > 0 ? 1 : 0);
+			//printf("width=%i x=%i y=%i val=%i\n", width, (i % width), (i / width), (val > 0 ? 1 : 0));
+			count++;
+			// update event matrix pixel values for those that have changed
+			PyList_SET_ITEM(matrix_ob, i, PyInt_FromLong(b));
+		}
+	}
+	// pack events in a list
+	PyObject* list = PyList_New(count);
+	for (i=0; i<count; i++) {
+		PyList_SET_ITEM(list, i, PyInt_FromLong(event[i]));
+	}
+	free(event);
+	//if (0 != count) printf("Generated %i events\n", count);
+	return list;
+}
+
+static char documentation[] = "Call method createEvents with two lists of equal length and the int width.";
+static char createEvents__doc__[] = "Call with two lists of equal length and the int width.";
+static char scaleImage__doc__[] = "Call with a list of int pixels and the int width; returns a list with the new image and the new width.";
+static struct PyMethodDef methods[] = {
+	{"createEvents", (PyCFunction)createEvents, 1, createEvents__doc__},
+	{"scaleImage", (PyCFunction)scaleImage, 1, scaleImage__doc__}
+};
+
+PyMODINIT_FUNC
+initEventGenerator(void)
+{
+	PyObject* m;
+	m = Py_InitModule4("EventGenerator", methods, documentation, (PyObject*)NULL, PYTHON_API_VERSION);
+	if (PyErr_Occurred()) {
+		Py_FatalError("\nCan't initialize module EventGenerator");
+	}
+}
+
