c2d3f9db25adb9f3ac851ad0054a671c6abc43fd
tobidelbruck <tobidelbruck@b7f4320f-462c-0410-a916-d9f35bb82d52>
2007-10-30 22:19:30 +0000

    added check to make sure Goalie is set to real time processing mode; also added propertychangesupport to filterchain to make sure the filterframe menu is uppdated properly to show this change.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@356 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/host/matlab/plotGoalie.m
+++ trunk/host/matlab/plotGoalie.m
@@ -1,7 +1,7 @@
 % plot goalie logging
 
 %% read data and plot data
-m=csvread('goalie.csv',2,0);
+m=csvread('corneroffsetmissing.csv',2,0);
 systimens=m(:,1);
 ballx=m(:,2);
 bally=m(:,3);
@@ -11,6 +11,7 @@ ballvelx=m(:,6);
 ballvely=m(:,7);
 ballxing=m(:,8);
 timestamps=m(:,9);
+eventrate=m(:,10);
 ind=find(timestamps==0);
 timestamps(ind)=NaN; % no events in these packets, no timestamp
 t0=min(timestamps);
@@ -19,16 +20,33 @@ systimems=systimens/1e6;
 time=systimems;
 
 figure(1);
+% subplot(211);
 plot182(time,armDesired, time, armActual, time, ballx,'x', time, bally, 's', time, ballxing,'o');
 legend('arm desired', 'arm actual', 'ballx', 'bally', 'ball crossing pixel');
 xlabel 'system time (ms)'
 ylabel 'position (pixels)'
 set(gca,'ylim',[-30,150]);
 
+% subplot(212);
+% plot(time,ballvelx,'x', time, ballvely,'o');
+% legend('ballvelx', 'ballvely');
+% xlabel 'system time (ms)'
+% ylabel 'pixels/second'
+% set(gca,'ylim',[-128/.01, 128/.01]);
+
+
 %%
 figure(2);
 difftimes=diff(systimems); % delta t's on host, ms according to System.nanoTime
-hist(difftimes,100);
-% plot(systimems,timestampsms);
+hist(difftimes,300);
 xlabel 'update interval on host (ms)'
-ylabel 'frequency'
+ylabel 'frequency'figure(3);
+shorttimes=difftimes(find(difftimes<8));
+mn=mean(shorttimes)
+md=median(shorttimes)
+
+% figure(3);
+% plot(systimems,timestampsms);
+% grid on
+% xlabel 'system time (ms)'
+% ylabel 'timestamp time (ms)'
