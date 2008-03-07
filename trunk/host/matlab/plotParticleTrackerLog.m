57e1cd032487b70820eb3c4a44cd70106ca13d42
hafliger <hafliger@b7f4320f-462c-0410-a916-d9f35bb82d52>
2008-03-07 11:09:01 +0000

    new function to quickly show an image extracted from a sequence of AEs
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@478 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/host/matlab/plotParticleTrackerLog.m
+++ trunk/host/matlab/plotParticleTrackerLog.m
@@ -1,16 +1,22 @@
-function plotParticleTrackerLog(frameN,time_shift,xshift,yshift,xscaling,yscaling,xlim,ylim)
-% Hydrolab: xshift=-62*366e-6; yshift=67*366e-6, xscaling=-366e-6,
-% yscaling=366e-6, xlim=[-70 70]*366e-6, ylim=xlim
+function plotParticleTrackerLog(logName,frameN,time_shift,xshift,yshift,xscaling,yscaling,xlim,ylim)
+% Hydrolab: xshift=-62*366e-6; yshift=67*366e-6; xscaling=-366e-6;
+% yscaling=366e-6; xlim=[-70 70]*366e-6; ylim=xlim;
 
-[particles]=ParticleTrackerLog(frameN+time_shift);
-if (~isempty(particles))
-x=xscaling*particles(:,2)-xshift;
-y=yscaling*particles(:,3)-yshift;
-u=xscaling*particles(:,4)
-v=yscaling*particles(:,5)
-plot(x,y,'o');
-hold on
-quiver(x,y,u*1e3,v*1e3,0);
-set(gca,'xlim',xlim,'ylim',ylim);
-hold off
-end; %if
+for i=frameN
+  [particles]=feval(logName,i+time_shift);
+  i
+  if (~isempty(particles))
+    x=xscaling*particles(:,2)-xshift;
+    y=yscaling*particles(:,3)-yshift;
+    u=xscaling*particles(:,4);
+    v=yscaling*particles(:,5);
+    plot(x,y,'o');
+    hold on
+    quiver(x,y,u*1e3,v*1e3,0);
+    set(gca,'xlim',xlim,'ylim',ylim);
+    hold off
+  else
+      cla;
+  end; %if
+  drawnow;
+end; %for
