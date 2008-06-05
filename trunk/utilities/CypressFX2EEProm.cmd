b5731b9ddb8c686e19c1a8aab5ecc0699ca182f7
tobidelbruck <tobidelbruck@b7f4320f-462c-0410-a916-d9f35bb82d52>
2008-06-05 14:37:59 +0000

    fixed java.library.path for eeprom downloader
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@624 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/utilities/CypressFX2EEProm.cmd
+++ trunk/utilities/CypressFX2EEProm.cmd
@@ -4,7 +4,7 @@ rem cd "C:\Documents and Settings\tobi\My Documents\avlsi-svn\CAVIAR\wp5\USBAER\
 cd ..\host\java
 
 rem add the java native path components
-PATH=%PATH%;%CD%\JNI;%CD%\JNI\SiLabsNativeWindows
+PATH=%PATH%;%CD%\jars;%CD%\jars\SiLabsNativeWindows
 
 java -Xms100m -Xmx400m -DJOGL_SINGLE_THREADED_WORKAROUND=true -Dsun.java2d.opengl=false -Dsun.java2d.noddraw=true -cp dist\jAER.jar;jars\usbiojava.jar;jars\swing-layout-0.9.jar;jars\jogl.jar ch.unizh.ini.caviar.hardwareinterface.usb.CypressFX2EEPROM
 pause
