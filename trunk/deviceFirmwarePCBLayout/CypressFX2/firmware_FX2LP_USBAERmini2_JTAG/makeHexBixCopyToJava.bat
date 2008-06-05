fd7f007c9d947c5bb843f6c825de7354543718b1
bernerr <bernerr@b7f4320f-462c-0410-a916-d9f35bb82d52>
2008-06-05 08:43:39 +0000

    added JTAG support for retina board, not fully functional yet
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@622 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/deviceFirmwarePCBLayout/CypressFX2/firmware_FX2LP_USBAERmini2_JTAG/makeHexBixCopyToJava.bat
+++ trunk/deviceFirmwarePCBLayout/CypressFX2/firmware_FX2LP_USBAERmini2_JTAG/makeHexBixCopyToJava.bat
@@ -1,9 +1,9 @@
 rem ******************** make hex and bix files
-hex2bix -b -R -M 8000 -o USBAERmini2.bix USBAERmini2.hex
 hex2bix -i -M 16000 -f 0xC2 -o USBAERmini2.iic USBAERmini2.hex
 
 rem ******************** copy bix firmware download file to java package folder for integration into jar file
 
-copy USBAERmini2.bix ..\..\..\host\java\src\ch\unizh\ini\caviar\hardwareinterface\usb\USBAERmini2.bix
+GOTO skip
 copy USBAERmini2.hex ..\..\..\host\java\src\ch\unizh\ini\caviar\hardwareinterface\usb\USBAERmini2.hex
 copy USBAERmini2.iic ..\..\..\host\java\src\ch\unizh\ini\caviar\hardwareinterface\usb\USBAERmini2.iic
+:skip
