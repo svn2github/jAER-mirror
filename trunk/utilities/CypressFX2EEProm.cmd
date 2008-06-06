cbad34ba7e35c505bf04bd62d37971fc5a4d6a6b
tobidelbruck <tobidelbruck@b7f4320f-462c-0410-a916-d9f35bb82d52>
2008-06-06 10:32:12 +0000

    removed unneccessary switches for jvm for the EEPROM program
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@626 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/utilities/CypressFX2EEProm.cmd
+++ trunk/utilities/CypressFX2EEProm.cmd
@@ -6,5 +6,5 @@ cd ..\host\java
 rem add the java native path components
 PATH=%PATH%;%CD%\jars;%CD%\jars\SiLabsNativeWindows
 
-java -Xms100m -Xmx400m -DJOGL_SINGLE_THREADED_WORKAROUND=true -Dsun.java2d.opengl=false -Dsun.java2d.noddraw=true -cp dist\jAER.jar;jars\usbiojava.jar;jars\swing-layout-0.9.jar;jars\jogl.jar ch.unizh.ini.caviar.hardwareinterface.usb.CypressFX2EEPROM
+java -cp dist\jAER.jar;jars\usbiojava.jar;jars\swing-layout-0.9.jar;jars\jogl.jar ch.unizh.ini.caviar.hardwareinterface.usb.CypressFX2EEPROM
 pause
