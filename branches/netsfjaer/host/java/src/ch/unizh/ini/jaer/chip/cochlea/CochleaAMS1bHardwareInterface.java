d516a9367c45a1c68f3a35c870253d2a300291df
tobidelbruck <tobidelbruck@b7f4320f-462c-0410-a916-d9f35bb82d52>
2008-11-22 22:26:56 +0000

    override the super's AERreader to translate events properly
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@1044 b7f4320f-462c-0410-a916-d9f35bb82d52

--- branches/netsfjaer/host/java/src/ch/unizh/ini/jaer/chip/cochlea/CochleaAMS1bHardwareInterface.java
+++ branches/netsfjaer/host/java/src/ch/unizh/ini/jaer/chip/cochlea/CochleaAMS1bHardwareInterface.java
@@ -2,9 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ch.unizh.ini.jaer.chip.cochlea;
 
+import de.thesycon.usbio.UsbIoBuf;
 import net.sf.jaer.biasgen.Biasgen;
 import net.sf.jaer.biasgen.BiasgenHardwareInterface;
 import net.sf.jaer.hardwareinterface.HardwareInterfaceException;
@@ -17,14 +17,13 @@ import net.sf.jaer.hardwareinterface.usb.cypressfx2.CypressFX2MonitorSequencer;
  * 
  * @author tobi
  */
-public class CochleaAMS1bHardwareInterface extends CypressFX2MonitorSequencer implements BiasgenHardwareInterface{
+public class CochleaAMS1bHardwareInterface extends CypressFX2MonitorSequencer implements BiasgenHardwareInterface {
 
-    final byte VR_CONFIG=CypressFX2.VENDOR_REQUEST_SEND_BIAS_BYTES;
-    
+    final byte VR_CONFIG = CypressFX2.VENDOR_REQUEST_SEND_BIAS_BYTES;
     /** The USB PID */
-    public static final short PID=(short)0x8405;
-    
-    public CochleaAMS1bHardwareInterface(int n){
+    public static final short PID = (short) 0x8405;
+
+    public CochleaAMS1bHardwareInterface(int n) {
         super(n);
     }
 
@@ -33,11 +32,11 @@ public class CochleaAMS1bHardwareInterface extends CypressFX2MonitorSequencer im
     }
 
     public void sendConfiguration(Biasgen biasgen) throws HardwareInterfaceException {
-        if(!(biasgen instanceof CochleaAMS1b.Biasgen)){
+        if (!(biasgen instanceof CochleaAMS1b.Biasgen)) {
             log.warning("biasgen is not instanceof CochleaAMS1b.Biasgen");
             return;
         }
-        ((CochleaAMS1b.Biasgen)biasgen).sendConfiguration();
+        ((CochleaAMS1b.Biasgen) biasgen).sendConfiguration();
     }
 
     public void flashConfiguration(Biasgen biasgen) throws HardwareInterfaceException {
@@ -45,15 +44,36 @@ public class CochleaAMS1bHardwareInterface extends CypressFX2MonitorSequencer im
     }
 
     public byte[] formatConfigurationBytes(Biasgen biasgen) {
-        if(!(biasgen instanceof CochleaAMS1b.Biasgen)){
-            log.warning(biasgen+" is not instanceof CochleaAMS1b.Biasgen, returning null array");
+        if (!(biasgen instanceof CochleaAMS1b.Biasgen)) {
+            log.warning(biasgen + " is not instanceof CochleaAMS1b.Biasgen, returning null array");
             return null;
         }
-        CochleaAMS1b.Biasgen b=(CochleaAMS1b.Biasgen) biasgen;
+        CochleaAMS1b.Biasgen b = (CochleaAMS1b.Biasgen) biasgen;
         return new byte[0];
     }
-    
-    public void update(Observable obs, Object obj){
-        log.info("got update from "+obs+" of "+obj);
+
+    /** 
+     * Starts reader buffer pool thread and enables in endpoints for AEs. This method is overridden to construct
+    our own reader with its translateEvents method
+     */
+    @Override
+    public void startAEReader() throws HardwareInterfaceException {  // raphael: changed from private to protected, because i need to access this method
+        setAeReader(new AEReader(this));
+        allocateAEBuffers();
+        getAeReader().startThread(3); // arg is number of errors before giving up
+        HardwareInterfaceException.clearException();
+    }
+
+    /** This reader understands the format of raw USB data and translates to the AEPacketRaw */
+    public class AEReader extends CypressFX2MonitorSequencer.MonSeqAEReader {
+
+        public AEReader(CypressFX2 cypress) throws HardwareInterfaceException {
+            super(cypress);
+        }
+
+        @Override
+        protected void translateEvents(UsbIoBuf b) {
+            translateEventsWithCPLDEventCode(b);
+        }
     }
 }
