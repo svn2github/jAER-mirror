51df5b7c6b2d7be158cfd4639f0f8eb42906c673
tobidelbruck <tobidelbruck@b7f4320f-462c-0410-a916-d9f35bb82d52>
2008-11-22 12:20:16 +0000

    fixed paths to new src tree
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@1039 b7f4320f-462c-0410-a916-d9f35bb82d52

--- branches/netsfjaer/deviceFirmwarePCBLayout/CypressFX2/firmware_FX2LP_Cochleaams1b/makeHexBixCopyToJava.cmd
+++ branches/netsfjaer/deviceFirmwarePCBLayout/CypressFX2/firmware_FX2LP_Cochleaams1b/makeHexBixCopyToJava.cmd
@@ -3,7 +3,7 @@ rem ******************** make binary iic download file from hex project output a
 @echo on
 hex2bix -i -M 16384 -F 0xC2 -o firmwareFX2_Cochleaams1b.iic firmwareFX2_Cochleaams1b.hex
 hex2bix -b -R -M 16384 -o firmwareFX2_Cochleaams1b.bix firmwareFX2_Cochleaams1b.hex
-copy firmwareFX2_Cochleaams1b.hex ..\..\..\host\java\src\sf\net\jaer\hardwareinterface\usb\cypressfx2\firmwareFX2_Cochleaams1b.hex
-copy firmwareFX2_Cochleaams1b.iic ..\..\..\host\java\src\sf\net\jaer\hardwareinterface\usb\cypressfx2\firmwareFX2_Cochleaams1b.iic
-copy firmwareFX2_Cochleaams1b.bix ..\..\..\host\java\src\sf\net\jaer\hardwareinterface\usb\cypressfx2\firmwareFX2_Cochleaams1b.bix
+copy firmwareFX2_Cochleaams1b.hex ..\..\..\host\java\src\net\sf\jaer\hardwareinterface\usb\cypressfx2\firmwareFX2_Cochleaams1b.hex
+copy firmwareFX2_Cochleaams1b.iic ..\..\..\host\java\src\net\sf\jaer\hardwareinterface\usb\cypressfx2\firmwareFX2_Cochleaams1b.iic
+copy firmwareFX2_Cochleaams1b.bix ..\..\..\host\java\src\net\sf\jaer\hardwareinterface\usb\cypressfx2\firmwareFX2_Cochleaams1b.bix
 
--- branches/netsfjaer/deviceFirmwarePCBLayout/CypressFX2/firmware_FX2LP_StereoBoard/makeHexBixCopyToJava.cmd
+++ branches/netsfjaer/deviceFirmwarePCBLayout/CypressFX2/firmware_FX2LP_StereoBoard/makeHexBixCopyToJava.cmd
@@ -3,6 +3,6 @@ hex2bix -i -f 0xC2 -o USBAERmini2.iic USBAERmini2.hex
 
 rem ******************** copy bix firmware download file to java package folder for integration into jar file
 
-copy USBAERmini2.hex ..\..\..\host\java\src\sf\net\jaer\hardwareinterface\usb\cypressfx2\TMPdiffStereo.hex
-copy USBAERmini2.iic ..\..\..\host\java\src\sf\net\jaer\hardwareinterface\usb\cypressfx2\TMPdiffStereo.iic
+copy USBAERmini2.hex ..\..\..\host\java\src\net\sf\jaer\hardwareinterface\usb\cypressfx2\TMPdiffStereo.hex
+copy USBAERmini2.iic ..\..\..\host\java\src\net\sf\jaer\hardwareinterface\usb\cypressfx2\TMPdiffStereo.iic
 pause
