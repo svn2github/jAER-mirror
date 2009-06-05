e2173f3f94aec32543d4b93d6a0fa45e59125a5c
tobidelbruck <tobidelbruck@b7f4320f-462c-0410-a916-d9f35bb82d52>
2009-06-05 12:13:07 +0000

    fixed \sf\net to \net\sf in copy to jaer code path
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@1429 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/deviceFirmwarePCBLayout/Cochleaams1bCPLD/XilinxProject/makeXSVFCopyToJava.cmd
+++ trunk/deviceFirmwarePCBLayout/Cochleaams1bCPLD/XilinxProject/makeXSVFCopyToJava.cmd
@@ -4,5 +4,5 @@ rem ******************** copy xsvf firmware download file to java package folder
 
 iMPACT -batch impact_commands.cmd
 
-copy Cochleaams1bCPLD.xsvf ..\..\..\host\java\src\sf\net\jaer\hardwareinterface\usb\cypressfx2\Cochleaams1bCPLD.xsvf
+copy Cochleaams1bCPLD.xsvf ..\..\..\host\java\src\net\sf\jaer\hardwareinterface\usb\cypressfx2\Cochleaams1bCPLD.xsvf
 pause
