928a3f3b0734fc60487fa32109c86a1d950207e2
holgerfinger <holgerfinger@b7f4320f-462c-0410-a916-d9f35bb82d52>
2009-06-11 17:59:21 +0000

    Added the matlab files to compute the frequency response of the AMS1b chip.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@1457 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/host/matlab/cochlea/FrequencyResponse/AMS1b/computeFreqRespAMS1b.m
+++ trunk/host/matlab/cochlea/FrequencyResponse/AMS1b/computeFreqRespAMS1b.m
@@ -1,72 +1,96 @@
-function [ freqResponse ] = computeFreqRespAMS1b( calibrationname, frequencies, signallength, record, evaluate )
+function [ freqResponse ] = freqResponseAMS1b( calibrationname, frequencies, volumes, signallength, doRecord, doEvaluate, doPlot )
+%COMPUTEFREQRESPAMS1B Summary of this function goes here
+
+%Call the function without arguments to plot a previously stored freqResponse
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Otherwise:
 %calibrationname is a string used for the foldername to store the files
 %frequencies is a vector containing the frequencies to test (in Hz)
+%volumes is a vector containing the volume levels to play
 %signallength is the playtime of every frequency (in seconds)
-%record=0 -> don't record
-%evaluate=0 -> don't evaluate the frequencies
+%doRecord=0 -> don't record
+%doEvaluate=0 -> don't evaluate the frequencies
+%doPlot=0 -> don't plot
 
-mkdir(['Recording/' calibrationname])
-Fs=16000;
+
+if nargin==0,
+    [filename,path]=uigetfile('*.mat','Select file');
+    if filename==0, return; end
+    load([path,filename]);
+    doRecord=0;
+    doEvaluate=0;
+    doPlot=1;
+end
+
 numOfCochleaChannels=64;
 
-if record
+if doRecord
+    mkdir(['Recording/' calibrationname])
+    Fs=16000;
+    
     %Open connection to jAER:
     u=udp('localhost',8997);
     fopen(u);
     
-    for trial=1:length(frequencies)
-        signal=sin((1:signallength*Fs)*2*pi*frequencies(trial)/(signallength*Fs));
-        commandJAER(u,['startlogging ' pwd '\Recording\' calibrationname '\ITD' num2str(frequencies(trial)) '.dat'])
-        pause(0.5);
-        fprintf('playing now sine wave with %d Hz, %d measurments left \n', frequencies(trial), length(frequencies)-trial);
-        soundsc(signal,Fs);
-        pause(signallength+1);
-        commandJAER(u,'stoplogging')
-        pause(0.5);
+    for indFrequency=1:length(frequencies)
+        for indVolume=1:length(volumes)
+            signal=sin((1:signallength*Fs)*2*pi*frequencies(indFrequency)/(signallength*Fs));
+			fprintf(u,['startlogging ' pwd '\Recording\' calibrationname '\Freq' num2str(indFrequency) 'Vol ' num2str(indVolume) '.dat']);
+			fprintf('%s',fscanf(u));
+            pause(0.5);
+            fprintf('playing now sine wave with %d Hz and %d volume, %d measurments left \n', frequencies(indFrequency), volumes(indVolume), (length(frequencies)-indFrequency)*length(volumes)-indVolume);
+            sound(signal*volumes(indVolume),Fs);
+            pause(signallength+1);
+            fprintf(u,'stoplogging');
+			fprintf('%s',fscanf(u));
+			pause(0.5);
+        end
     end
     % clean up the UDP connection to jAER:
     fclose(u);
     delete(u);
     clear u
+    save(['Recording\' calibrationname '\settings']);
 end
 
-if evaluate
-    freqResponse=cell(4,2,2); % Neuron, FilterType, Side
+if doEvaluate
+    freqResponse=cell(4,2,2,length(volumes)); % Neuron, FilterType, Side
     for neuron=1:4
-        for filtertype=1:2
-            for side=1:2
-                freqResponse{neuron,filtertype,side}=zeros(length(frequencies),numOfCochleaChannels);
+        for side=1:2
+            for indVolume=1:length(volumes)
+                freqResponse{neuron,side,indVolume}=zeros(length(frequencies),numOfCochleaChannels);
             end
         end
     end
-    
-    for trial=1:length(frequencies)
-        [trialAddr,trialTs]=loadaerdat([pwd '\Recording\' calibrationname '\ITD' num2str(frequencies(trial)) '.dat']);
-        [trialChan, trialNeuron, trialFilterType, trialSide]=extractCochleaEventsFromAddrAMS1b(trialAddr);
-        
-        for chan=1:numOfCochleaChannels
-            for neuron=1:4
-                for filtertype=1:2
+    for indFrequency=1:length(frequencies)
+        for indVolume=1:length(volumes)
+            [trialAddr]=loadaerdat([pwd '\Recording\' calibrationname '\Freq' num2str(indFrequency) 'Vol ' num2str(indVolume) '.dat']);
+            [trialChan, trialNeuron, trialFilterType, trialSide]=extractAMS1bEventsFromAddr(trialAddr);
+            fprintf('evaluating now %d Hz and %d volume, %d left \n', frequencies(indFrequency), volumes(indVolume), (length(frequencies)-indFrequency)*length(volumes)-indVolume);
+            for chan=1:numOfCochleaChannels
+                for neuron=1:4
                     for side=1:2
-                        freqResponse{neuron,filtertype,side}(trial,chan)=...
-                            length(find(trialChan==chan & trialNeuron==neuron & trialFilterType==filtertype & trialSide==side));
+                        freqResponse{neuron,side,indVolume}(indFrequency,chan)=length(find(trialChan==chan & trialNeuron==neuron & trialSide==side))/signallength;
                     end
                 end
             end
         end
     end
-    
-    figure(1)
-    for neuron=1:4
-        for filtertype=1:2
-            for side=1:2
-                subplot(4,4,neuron+filtertype*4+side*8)
-                surf(1:numOfCochleaChannels,frequencies,freqResponse{neuron,filtertype,side})
+    save(['Recording\' calibrationname '\freqResponse']);
+end
+
+if doPlot
+    for indVolume=1:length(volumes)
+        for side=1:2
+            figure()
+            for neuron=1:4
+                subplot(2,2,neuron)
+                surf(1:numOfCochleaChannels,frequencies,freqResponse{neuron,side,indVolume})
                 set(gca, 'YScale', 'log')
-                ylabel('Played Frequency');
-                xlabel('Cochlea Channel');
-                zlabel('Response');
-                title(['Neuron=' neuron ' filtertype=' filtertype 'side=' side]);
+                ylabel('frequency');
+                xlabel('channel');
+                zlabel('spikes/sec');
+                title(['neuron=' num2str(neuron) ' volume=' num2str(volumes(indVolume))]);
             end
         end
     end
