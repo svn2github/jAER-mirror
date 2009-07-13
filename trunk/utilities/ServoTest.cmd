0cfcd462f7963b71909a97dbabd658d58be06843
tobidelbruck <tobidelbruck@b7f4320f-462c-0410-a916-d9f35bb82d52>
2009-07-13 04:30:10 +0000

    updated path and JNI path to make servotest work
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@1561 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/utilities/ServoTest.cmd
+++ trunk/utilities/ServoTest.cmd
@@ -6,7 +6,7 @@ cd ..\host\java
 rem the java native code MUST be on windows PATH, not just on java.library.path. The SiLabs JNI DLL wont find the SiUSBXp.dll if it is not on Windows PATH 
 PATH=%PATH%;%CD%\jars;%CD%\jars\SiLabsNativeWindows
 
-rem -Djava.library.path="%CD%\JNI;%CD%\JNI\SiLabsNativeWindows"
+rem -Djava.library.path="%CD%\jars;%CD%\jars\SiLabsNativeWindows"
 
-java -Xms100m -Xmx512m  -DJOGL_SINGLE_THREADED_WORKAROUND=false -Dsun.java2d.opengl=false -Dsun.java2d.noddraw=true -cp dist\jAER.jar;jars\usbiojava.jar;jars\swing-layout-0.9.jar;jars\jogl.jar ch.unizh.ini.caviar.hardwareinterface.usb.ServoTest %1
+java -Xms100m -Xmx512m  -DJOGL_SINGLE_THREADED_WORKAROUND=false -Dsun.java2d.opengl=false -Dsun.java2d.noddraw=true -cp dist\jAER.jar;jars\usbiojava.jar;jars\swing-layout-0.9.jar;jars\jogl.jar net.sf.jaer.hardwareinterface.usb.ServoTest %1
 pause
