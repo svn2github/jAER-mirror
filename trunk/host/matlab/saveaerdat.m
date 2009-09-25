790796d7d7708bc90a882f2698c3436c52814eed
tobidelbruck <tobidelbruck@b7f4320f-462c-0410-a916-d9f35bb82d52>
2009-09-25 11:59:37 +0000

    more comments
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@1651 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/host/matlab/saveaerdat.m
+++ trunk/host/matlab/saveaerdat.m
@@ -1,10 +1,17 @@
 function saveaerdat(train,filename)
 % function saveaerdat(train[,filename])
-% write events to a .dat file (tobi's aer data format)
+% write events to a .dat file (tobi's aer data format).
 % run this script, which opens a file browser. browse to the .dat file and click "Open".
 %
-% argument train is the data, an Nx2 array
-% train(:,1) are the timestamps with 1us tick, train(:,2) are the addresses
+% argument train is the data, an Nx2 array.
+% train(:,1) are the timestamps with 1us tick, train(:,2) are the
+% addresses.
+% these address are raw; to generate addressses corresponding to a
+% particular x,y location and event type, you need to know the bit mapping.
+% For instance, for the DVS128, the addresses are 15 bit, with 
+% AE15=0, AE14:8=y, AE7:1=x, and AE0=polarity of event. See
+% extractRetina128EventsFromAddress.m.
+%
 % filename is an optional filename which overrides the dialog box
 
 if nargin==1,
