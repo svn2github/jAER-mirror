6ee9b0d486083d006587440c203d4ae6eaf65731
tobidelbruck <tobidelbruck@b7f4320f-462c-0410-a916-d9f35bb82d52>
2010-06-10 10:40:37 +0000

    signed 64 bit driver and moved some more files around
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@1996 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ trunk/drivers/windows/driverDVS_USBAERmini2/x32/signdriver.cmd
@@ -0,0 +1,1 @@
+rem signs drivers
--- trunk/drivers/windows/driverSigning/signDrivers.cmd
+++ trunk/drivers/windows/driverSigning/signDrivers.cmd
@@ -2,15 +2,15 @@ rem signing USB drivers for windows using stored iniLabs GmbH certificate
 
 rem signing 32 bit driver
 
-set r=..\driverDVS128AndOthersSigned\
+set root=..\driverDVS_USBAERmini2
 
-set d=..\driverDVS128AndOthersSigned\x32
-Inf2Cat.exe /driver:%d% /os:XP_X86,Vista_X86,7_X86 /verbose
-signtool sign /v /n "iniLabs GmbH" /ac MSCV-GlobalSign.cer /t http://timestamp.verisign.com/scripts/timestamp.dll %d%\usb2aemon.cat
+rem set d=%root%\x32
+rem Inf2Cat.exe /driver:%d% /os:XP_X86,Vista_X86,7_X86 /verbose
+rem signtool sign /v /n "iniLabs GmbH" /ac MSCV-GlobalSign.cer /t http://timestamp.verisign.com/scripts/timestamp.dll %d%\usb2aemon.cat
 
 rem signing 64 bit driver
 
-set d=..\driverDVS128AndOthersSigned\x64
+set d=%root%\x64
 Inf2Cat.exe /driver:%d% /os:XP_X86,Vista_X86,7_X86 /verbose
-signtool sign /v /n "iniLabs GmbH" /ac MSCV-GlobalSign.cer /t http://timestamp.verisign.com/scripts/timestamp.dll %d%\usb2aemon.cat
+signtool sign /v /n "iniLabs GmbH" /ac MSCV-GlobalSign.cer /t http://timestamp.verisign.com/scripts/timestamp.dll %d%\usb2aemon_x64.cat
 
