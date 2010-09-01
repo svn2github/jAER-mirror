e1958a5f6b4755521d1d952b3b9216ab73ac7bd6
braendch <braendch@b7f4320f-462c-0410-a916-d9f35bb82d52>
2010-09-01 15:16:47 +0000

    Improved version of the generateRandomTunnelTraffic.m that initializes the used matrices which makes the algorithm much faster. New timestamping algorithm that avoids timestamp bulbs. Addition of generateRandomBodyTraffic.m that allows to create random traffic of b/w bodies drawn in body.gif (which is transformed into suitable coordinates by generateBody.m)
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@2258 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/host/matlab/MultiUDPCamera/generateRandomBodyTraffic.m
+++ trunk/host/matlab/MultiUDPCamera/generateRandomBodyTraffic.m
@@ -1,21 +1,24 @@
-function generateRandomTunnelTraffic()
+function generateRandomBodyTraffic()
 dsx = 1280;
 dsy = 128;
 xshift=1; % bits to shift x to left
 yshift=12; % bits to shift y to right
 
-filename = strcat('randomTraffic_',datestr(now,'yyyy-mm-ddTHH-MM-SS'),'.aedat');
+filename = strcat('randomBodyTraffic_',datestr(now,'yyyy-mm-ddTHH-MM-SS'),'.aedat');
+
+% body = [-1 0 1 2 2 2 2 2 2  2  2  2  2  1  0 -1 -2 -2 -2 -2 -2 -2 -2 -2 -2 -2 -1 0 1 -1  0  1
+%         6  6 6 5 4 3 2 1 0 -1 -2 -3 -4 -5 -5 -5 -4 -3 -2 -1  0  1  2  3  4  5  2 3 2 -1 -2 -1];
+body = generateBody();
 
 seq_length = 30000; %sequence length in ms
 ts_resolution = 80; %how often a point has the chance to create a spike [in ms]
 nr_agents = 20;
 agent_start_x = ((rand(nr_agents,1)-0.5)*2)*dsx;
 agent_start_y = rand(nr_agents,1)*dsy;
-max_speed = 0.03;
+max_speed = 0.02;
 agent_speed = max_speed*rand(nr_agents,1);
 agent_freq = 1+(rand(nr_agents,1)-0.5);
 agent_amplitude = (rand(nr_agents,1)-0.5)*2;
-length = 15;
 ev_per_contour = 0.8;
 
 spikes_count = 1;
@@ -25,16 +28,16 @@ while base_ts < seq_length*1000 %us
     base_ts = round(base_ts+ts_resolution*1000);
     spike_array = zeros(max_elements, 2);
     for a=1:nr_agents
-        %circle for head
-        for alpha=0:10*2*pi
-           if rand<ev_per_contour*abs(sin(alpha/10))*(agent_speed(a)/max_speed)
+        %body
+        for index=1:size(body,2)
+           if rand<ev_per_contour*(agent_speed(a)/max_speed)
                ts = base_ts+rand*ts_resolution;
                center_x = agent_start_x(a)+ts*agent_speed(a)/(1000);
                center_y = agent_start_y(a)+agent_amplitude(a)*dsy/2*(1+sin(agent_freq(a)*2*pi*center_x/dsx));
-               x = round(center_x+length*sin(alpha/10));
-               y = round(center_y+length*cos(alpha/10));
+               x = round(center_x+body(1,index));
+               y = round(center_y+body(2,index));
                if (x>=0 && y>=0 && x<dsx && y<dsy)
-                   if sin(alpha/10)>0 
+                   if body(1,index)>=0 
                        pol=0;
                    else
                        pol=1;
