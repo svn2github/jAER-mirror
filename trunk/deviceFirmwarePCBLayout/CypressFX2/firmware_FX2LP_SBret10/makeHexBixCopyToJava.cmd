1e6fc7a1845edf9d5ab23bd06f7335a8b4686da9
braendch <braendch@b7f4320f-462c-0410-a916-d9f35bb82d52>
2012-08-25 18:04:13 +0000

    makeHexBixCopyToJava.cmd updated and files compiled accordingly
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@3511 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/deviceFirmwarePCBLayout/CypressFX2/firmware_FX2LP_SBret10/makeHexBixCopyToJava.cmd
+++ trunk/deviceFirmwarePCBLayout/CypressFX2/firmware_FX2LP_SBret10/makeHexBixCopyToJava.cmd
@@ -2,8 +2,8 @@
 rem ******************** make binary I2C download file from hex project output
 rem we are still using .bix file format because the download definitely works with it from jAER
 @echo on
-hex2bix -i -M 14592 -F 0xC2 -o SeeBetter20.iic SeeBetter20.hex
-hex2bix -b -R -M 14592 -o SeeBetter20.bix SeeBetter20.hex
+hex2bix -i -M 14592 -F 0xC2 -o SBret10.iic SBret10.hex
+hex2bix -b -R -M 14592 -o SBret10.bix SBret10.hex
 
 @echo on
 
