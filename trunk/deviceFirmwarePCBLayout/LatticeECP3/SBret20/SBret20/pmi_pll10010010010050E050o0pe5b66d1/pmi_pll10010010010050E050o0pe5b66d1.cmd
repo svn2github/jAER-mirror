099933d1fb006b1c3c7f0f0ca7e198b50d4ff3ae
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-04-02 13:17:46 +0000

    FX3 logic: remove obsolete.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@4575 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/deviceFirmwarePCBLayout/LatticeECP3/SBret20/SBret20/pmi_pll10010010010050E050o0pe5b66d1/pmi_pll10010010010050E050o0pe5b66d1.cmd
+++
@@ -1,3 +0,0 @@
-
-"C:/lscc/diamond/3.0_x64/ispfpga/bin/nt64/scuba" -w -arch ep5c00 -n pmi_pll10010010010050E050o0pe5b66d1 -bb -bus_exp 7 -type pll -fin 30 -fclkop 90 -fclkok 50  -fb_mode CLKOP -phaseadj 0 -duty 4.00000 -pmi
-
