dd9c66a9c77fe89ddf3a65b4e9e86d4a84e6ef5a
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-04-20 01:24:24 +0000

    Add StreamTester, to reliably reproduce the transfer problem to the FX3 fifo and explore solutions.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@4596 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ trunk/deviceFirmwarePCBLayout/LatticeECP3/StreamTester/StreamTester/source/fifoStatemachine.vhd
@@ -0,0 +1,111 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_ARITH.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
+
+entity fifoStatemachine is
+port (
+	Clock_CI               : in  std_logic;
+	Reset_RBI              : in  std_logic;
+	Run_SI              : in  std_logic;
+	
+	-- USB FIFO flags
+	USBFifoFull_SBI         : in  std_logic;
+	USBFifoAlmostFull_SBI   : in std_logic;
+	
+	-- Input FIFO flags
+	InFifoEmpty_SI              : in std_logic;
+
+	-- Input FIFO control lines
+	InFifoRead_SO : out std_logic;
+	
+	-- USB FIFO control lines
+	USBFifoChipSelect_SBO     : out std_logic;
+	USBFifoWrite_SBO          : out std_logic;
+	USBFifoPktEnd_SBO         : out std_logic;
+	USBFifoAddress_DO         : out std_logic_vector(1 downto 0));
+end fifoStatemachine;
+
+architecture Behavioral of fifoStatemachine is
+  type state is (stIdle, stSetupDelay1, stSetupDelay2, stSetupDelay3, stSetupWrite, stWrite);
+  -- present and next state
+  signal State_DP, State_DN : state;
+
+  -- FIFO addresse: always write to socket 0, which is the output socket for FPGA->FX3 USB.
+  constant EP_FIFO : std_logic_vector := "00";
+  
+	begin
+
+	-- calculate next state and outputs
+	  p_memless : process (State_DP, USBFifoFull_SBI, USBFifoAlmostFull_SBI, InFifoEmpty_SI, Run_SI)
+	  begin  -- process p_memless
+		-- default assignements: stay in present state, don't change address in
+		-- FifoAddress register, write registers for transfers
+		State_DN                     <= State_DP;
+
+		USBFifoChipSelect_SBO        <= '0'; -- Always keep chip selected (active-low).
+		USBFifoWrite_SBO             <= '1';
+		USBFifoPktEnd_SBO            <= '1';
+		USBFifoAddress_DO            <= EP_FIFO;
+
+		InFifoRead_SO <= '0'; -- Don't read from input FIFO until we know we can write.
+	
+		case State_DP is
+		  when stIdle =>
+			if InFifoEmpty_SI = '0' and USBFifoFull_SBI = '1' and Run_SI = '1' then
+			  State_DN <= stSetupDelay1;
+			end if;
+		 
+		 -- Wait three cycles so that the USBFifoFull flag has time to assert itself.
+		  when stSetupDelay1 =>
+				State_DN <= stSetupDelay2;
+			  
+		  when stSetupDelay2 =>
+				State_DN <= stSetupDelay3;
+			  
+		  when stSetupDelay3 =>
+				State_DN <= stSetupWrite;
+
+		  when stSetupWrite =>
+			-- Check now, after delaying, that the FIFO is still free.
+			if USBFifoFull_SBI = '1' then
+			  State_DN <= stWrite;
+			  InFifoRead_SO <= '1';
+			else
+			  State_DN <= stIdle;
+			end if;
+		  
+		  -- Write the data to the USB FIFO continuously, until either the input FIFO is
+		  -- empty or the USB one is full.
+		  when stWrite =>
+			-- Check that there still is data to send.
+			if  InFifoEmpty_SI = '1' then
+			  State_DN <= stIdle;
+			end if;
+			
+			-- Check that we're reaching the end of the FIFO using the watermark flag (almost full).
+			-- This way we know exactly how much space is left and can start to limit the number of
+			-- writes, without having to check the FULL flag, which has a 3-cycle delay.
+			if USBFifoAlmostFull_SBI = '0' then
+				State_DN <= stIdle; -- Single cycle access mode.
+			end if;
+
+			-- Execute write and continue to read next data chunk.
+			USBFifoWrite_SBO <= '0';
+			InFifoRead_SO <= '1';
+				
+		  when others => null;
+		end case;
+
+	  end process p_memless;
+
+	  -- Change state on clock edge (synchronous).
+	  p_memoryzing : process (Clock_CI, Reset_RBI)
+	  begin  -- process p_memoryzing
+		if Reset_RBI = '0' then             -- asynchronous reset (active low)
+		  State_DP <= stIdle;
+		elsif rising_edge(Clock_CI) then
+		  State_DP <= State_DN;
+		end if;
+	  end process p_memoryzing;
+	end Behavioral;
---
+++ trunk/deviceFirmwarePCBLayout/LatticeECP3/StreamTester/StreamTester/source/topLevel.vhd
@@ -0,0 +1,111 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_ARITH.all;
+use IEEE.numeric_std.all;
+use IEEE.STD_LOGIC_UNSIGNED."+";
+
+entity topLevel is
+  port (
+    USBClock_CI : in std_logic;
+    Reset_RBI : in std_logic;
+	FPGARun_SI : in std_logic;
+	
+	USBFifoData_DO : out std_logic_vector(15 downto 0);
+	USBFifoChipSelect_SBO : out std_logic;
+	USBFifoWrite_SBO : out std_logic;
+	USBFifoRead_SBO : out std_logic;
+	USBFifoPktEnd_SBO : out std_logic;
+	USBFifoAddress_DO : out std_logic_vector(1 downto 0);
+	USBFifoFull_SBI : in std_logic;
+	USBFifoAlmostFull_SBI : in std_logic;
+	
+	LED1_SO : out std_logic;
+	LED2_SO : out std_logic);
+end topLevel;
+
+architecture Structural of topLevel is
+  component fifoStatemachine
+    port (
+    Clock_CI : in  std_logic;
+	Reset_RBI : in  std_logic;
+	Run_SI : in  std_logic;
+	USBFifoFull_SBI : in  std_logic;
+	USBFifoAlmostFull_SBI : in std_logic;
+	InFifoEmpty_SI : in std_logic;
+    InFifoRead_SO : out std_logic;
+	USBFifoChipSelect_SBO : out std_logic;
+	USBFifoWrite_SBO : out std_logic;
+	USBFifoPktEnd_SBO : out std_logic;
+	USBFifoAddress_DO : out std_logic_vector(1 downto 0));
+  end component;
+
+  component AERfifo
+    port (
+    Data: in std_logic_vector(15 downto 0); 
+    WrClock: in std_logic; 
+    RdClock: in std_logic; 
+    WrEn: in std_logic; 
+    RdEn: in std_logic; 
+    Reset: in std_logic; 
+    RPReset: in std_logic; 
+    Q: out std_logic_vector(15 downto 0); 
+    Empty: out std_logic; 
+    Full: out std_logic; 
+    AlmostEmpty: out std_logic; 
+    AlmostFull: out std_logic);
+  end component;
+  
+  component continuousCounter
+    port (
+    Clock_CI : in  std_logic;
+    Reset_RBI : in  std_logic;
+    Data_DO : out std_logic_vector(15 downto 0));
+  end component;
+  
+  signal Reset_RI: std_logic;
+  signal AERFifoDataIn_D : std_logic_vector(15 downto 0);
+  signal AERFifoWrite_S, AERFifoRead_S : std_logic;
+  signal AERFifoEmpty_S, AERFifoFull_S : std_logic;
+begin
+  Reset_RI <= not Reset_RBI;
+  AERFifoWrite_S <= '1';
+  USBFifoRead_SBO <= '1';
+  LED1_SO <= FPGARun_SI;
+  LED2_SO <= '0';
+
+uFifoStatemachine: fifoStatemachine
+    port map (
+    Clock_CI => USBClock_CI,
+	Reset_RBI => Reset_RBI,
+	Run_SI => FPGARun_SI,
+	USBFifoFull_SBI => USBFifoFull_SBI,
+	USBFifoAlmostFull_SBI => USBFifoAlmostFull_SBI,
+	InFifoEmpty_SI => AERFifoEmpty_S,
+    InFifoRead_SO => AERFifoRead_S,
+	USBFifoChipSelect_SBO => USBFifoChipSelect_SBO,
+	USBFifoWrite_SBO => USBFifoWrite_SBO,
+	USBFifoPktEnd_SBO => USBFifoPktEnd_SBO,
+	USBFifoAddress_DO => USBFifoAddress_DO);
+	  
+  uFifo : AERfifo
+    port map (
+      Data => AERFifoDataIn_D,
+      WrClock => USBClock_CI,
+      RdClock => USBClock_CI,
+      WrEn => AERFifoWrite_S, 
+      RdEn => AERFifoRead_S,
+      Reset => Reset_RI,
+      RPReset => Reset_RI,
+      Q =>  USBFifoData_DO,
+      Empty => AERFifoEmpty_S, 
+      Full => AERFifoFull_S,
+      AlmostEmpty => open,
+      AlmostFull => open);
+
+  uContinuousCounter : continuousCounter
+    port map (
+    Clock_CI => USBClock_CI,
+    Reset_RBI => Reset_RBI,
+    Data_DO => AERFifoDataIn_D);
+
+end Structural;
