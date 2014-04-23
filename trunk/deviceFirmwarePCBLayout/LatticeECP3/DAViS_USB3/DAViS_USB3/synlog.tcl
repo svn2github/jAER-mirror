7db3787507cab1328fa10384cc80ed906512e099
vicente198 <vicente198@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-04-23 12:59:12 +0000

    git-svn-id: svn://svn.code.sf.net/p/jaer/code@4606 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/deviceFirmwarePCBLayout/LatticeECP3/DAViS_USB3/DAViS_USB3/synlog.tcl
+++ trunk/deviceFirmwarePCBLayout/LatticeECP3/DAViS_USB3/DAViS_USB3/synlog.tcl
@@ -1,1 +1,5 @@
-run_tcl -fg DAViS_USB3_DAViS_USB3_synplify.tcl
+source "C:/Users/Vicente/AppData/Local/Synplicity/scm_perforce.tcl"
+history clear
+run_tcl -fg C:/3jAER/deviceFirmwarePCBLayout/LatticeECP3/DAViS_USB3/DAViS_USB3/launch_synplify.tcl
+launch_system_designer
+project -close C:/3jAER/deviceFirmwarePCBLayout/LatticeECP3/DAViS_USB3/DAViS_USB3/DAViS_USB3_syn.prj
