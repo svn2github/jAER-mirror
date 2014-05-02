3e236063f5ffdad5a10dc3b0b09bbfb4ed858fb8
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-05-02 13:00:35 +0000

    LatticeECP3: switch to one-process FSM design for testing (still broken on AERfifo empty).
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@4631 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/deviceFirmwarePCBLayout/LatticeECP3/StreamTester/sourcecode/fifoStatemachine.vhd
+++ trunk/deviceFirmwarePCBLayout/LatticeECP3/StreamTester/sourcecode/fifoStatemachine.vhd
@@ -28,18 +28,19 @@ port (
 end fifoStatemachine;
 
 architecture Behavioral of fifoStatemachine is
-	type state is (stIdle0, stPrepareWrite0, stWrite0, stSwitch0_Select, stSwitch0_toIdle, stSwitch0_toWrite,
-	               stIdle1, stPrepareWrite1, stWrite1, stSwitch1_Select, stSwitch1_toIdle, stSwitch1_toWrite);
+	type state is (stIdle0, stPrepareWrite0, stWrite0, stSwitch0,
+	               stIdle1, stPrepareWrite1, stWrite1, stSwitch1);
+
 	-- present and next state
-	signal State_DP, State_DN : state;
+	signal State_DP: state;
 
 	begin
-		-- calculate next state and outputs
-		p_memless : process (State_DP, USBFifoThread0Full_SI, USBFifoThread0AlmostFull_SI, USBFifoThread1Full_SI, USBFifoThread1AlmostFull_SI, InFifoEmpty_SI, InFifoAlmostEmpty_SI, Run_SI)
-		begin -- process p_memless
-			-- default assignements: stay in present state, chip selected, don't write, access Thread 0, don't read from FIFO
-			State_DN <= State_DP;
-
+		-- Change state on clock edge (synchronous).
+		process (Clock_CI, Reset_RBI)
+		begin
+		if Reset_RBI = '0' then -- asynchronous reset (active-low)
+			State_DP <= stIdle0;
+		elsif rising_edge(Clock_CI) then
 			USBFifoChipSelect_SBO <= '0'; -- Always keep chip selected (active-low).
 			USBFifoWrite_SBO <= '1';
 			USBFifoPktEnd_SBO <= '1';
@@ -50,15 +51,11 @@ architecture Behavioral of fifoStatemachine is
 			case State_DP is
 			when stIdle0 =>
 				if Run_SI = '1' and InFifoAlmostEmpty_SI = '0' and USBFifoThread0Full_SI = '0' then
-					State_DN <= stPrepareWrite0;
+					State_DP <= stPrepareWrite0;
 				end if;
 
 			when stPrepareWrite0 =>
-				if USBFifoThread0AlmostFull_SI = '1' then
-					State_DN <= stSwitch0_Select;
-				else
-					State_DN <= stWrite0;
-				end if;
+				State_DP <= stWrite0;
 				InFifoRead_SO <= '1'; -- Signal we want to read from the FIFO on next cycle.
 
 			when stWrite0 =>
@@ -66,12 +63,12 @@ architecture Behavioral of fifoStatemachine is
 				-- This way we know exactly how much space is left (2 cycles) and can enter the right path
 				-- to switch to the next thread.
 				if USBFifoThread0AlmostFull_SI = '1' then
-					State_DN <= stSwitch0_Select;
+					State_DP <= stSwitch0;
 					USBFifoWrite_SBO <= '0';
 					InFifoRead_SO <= '1';
 				-- Check that there still is data to send.
 				elsif InFifoAlmostEmpty_SI = '1' then
-					State_DN <= stIdle0;
+					State_DP <= stIdle0;
 					-- Last piece of data is sampled when the empty flag goes high.
 					-- So we still write it out here.
 					USBFifoWrite_SBO <= '0';
@@ -81,41 +78,27 @@ architecture Behavioral of fifoStatemachine is
 					InFifoRead_SO <= '1';
 				end if;
 
-			when stSwitch0_Select =>
-				if USBFifoThread1Full_SI = '1' or InFifoAlmostEmpty_SI = '1' then
-					State_DN <= stSwitch0_toIdle;
+			when stSwitch0 =>
+				if USBFifoThread1Full_SI = '1' or InFifoEmpty_SI = '1' then
+					State_DP <= stIdle1;
 				else
-					State_DN <= stSwitch0_toWrite;
+					State_DP <= stWrite1;
+					InFifoRead_SO <= '1';
 				end if;
 
 				USBFifoWrite_SBO <= '0';
-				InFifoRead_SO <= '1';
-
-			when stSwitch0_toIdle =>
-				State_DN <= stIdle1;
-
-				USBFifoWrite_SBO <= '0';
-
-			when stSwitch0_toWrite =>
-				State_DN <= stWrite1;
-				
-				USBFifoWrite_SBO <= '0';
-				InFifoRead_SO <= '1';
 
 			when stIdle1 =>
 				USBFifoAddress_DO(0) <= '1'; -- Access Thread 1.
+
 				if Run_SI = '1' and InFifoAlmostEmpty_SI = '0' and USBFifoThread1Full_SI = '0' then
-					State_DN <= stPrepareWrite1;
+					State_DP <= stPrepareWrite1;
 				end if;
 
 			when stPrepareWrite1 =>
 				USBFifoAddress_DO(0) <= '1'; -- Access Thread 1.
 
-				if USBFifoThread1AlmostFull_SI = '1' then
-					State_DN <= stSwitch1_Select;
-				else
-					State_DN <= stWrite1;
-				end if;
+				State_DP <= stWrite1;
 				InFifoRead_SO <= '1'; -- Signal we want to read from the FIFO on next cycle.
 
 			when stWrite1 =>
@@ -125,12 +108,12 @@ architecture Behavioral of fifoStatemachine is
 				-- This way we know exactly how much space is left (2 cycles) and can enter the right path
 				-- to switch to the next thread.
 				if USBFifoThread1AlmostFull_SI = '1' then
-					State_DN <= stSwitch1_Select;
+					State_DP <= stSwitch1;
 					USBFifoWrite_SBO <= '0';
 					InFifoRead_SO <= '1';
 				-- Check that there still is data to send.
 				elsif InFifoAlmostEmpty_SI = '1' then
-					State_DN <= stIdle1;
+					State_DP <= stIdle1;
 					-- Last piece of data is sampled when the empty flag goes high.
 					-- So we still write it out here.
 					USBFifoWrite_SBO <= '0';
@@ -140,42 +123,20 @@ architecture Behavioral of fifoStatemachine is
 					InFifoRead_SO <= '1';
 				end if;
 
-			when stSwitch1_Select =>
+			when stSwitch1 =>
 				USBFifoAddress_DO(0) <= '1'; -- Access Thread 1.
 
-				if USBFifoThread0Full_SI = '1' or InFifoAlmostEmpty_SI = '1' then
-					State_DN <= stSwitch1_toIdle;
+				if USBFifoThread0Full_SI = '1' or InFifoEmpty_SI = '1' then
+					State_DP <= stIdle0;
 				else
-					State_DN <= stSwitch1_toWrite;
+					State_DP <= stWrite0;
+					InFifoRead_SO <= '1';
 				end if;
 
 				USBFifoWrite_SBO <= '0';
-				InFifoRead_SO <= '1';
-
-			when stSwitch1_toIdle =>
-				USBFifoAddress_DO(0) <= '1'; -- Access Thread 1.
-				State_DN <= stIdle0;
-
-				USBFifoWrite_SBO <= '0';
-
-			when stSwitch1_toWrite =>
-				USBFifoAddress_DO(0) <= '1'; -- Access Thread 1.
-				State_DN <= stWrite0;
-
-				USBFifoWrite_SBO <= '0';
-				InFifoRead_SO <= '1';
 
 			when others => null;
-		end case;
-	end process p_memless;
-
-	-- Change state on clock edge (synchronous).
-	p_memoryzing : process (Clock_CI, Reset_RBI)
-	begin -- process p_memoryzing
-		if Reset_RBI = '0' then -- asynchronous reset (active-low)
-			State_DP <= stIdle0;
-		elsif rising_edge(Clock_CI) then
-			State_DP <= State_DN;
+			end case;
 		end if;
-	end process p_memoryzing;
+	end process;
 end Behavioral;
