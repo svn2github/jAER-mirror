3444602e786124b4caf06fba0d27ab3281ca8ff9
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-06-09 16:26:17 +0000

    SeeBetterLogic: add DVS AER bus state machine for getting data off the AER bus coming from the chip.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@4762 b7f4320f-462c-0410-a916-d9f35bb82d52

--- devices/logic/LatticeECP3/SeeBetterLogic/sourcecode/DVSAERStateMachine.vhd
+++ devices/logic/LatticeECP3/SeeBetterLogic/sourcecode/DVSAERStateMachine.vhd
@@ -1,5 +1,5 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
 use work.Settings.all;
 
 entity DVSAERStateMachine is
@@ -21,7 +21,78 @@ entity DVSAERStateMachine is
 end DVSAERStateMachine;
 
 architecture Behavioral of DVSAERStateMachine is
+	type state is (stIdle, stWriteAddr, stAck);
 
+	attribute syn_enum_encoding			 : string;
+	attribute syn_enum_encoding of state : type is "onehot";
+
+	-- present and next state
+	signal State_DP, State_DN : state;
 begin
+	p_memoryless : process (State_DP, DVSRun_SI, OutFifoFull_SI, OutFifoAlmostFull_SI, DVSAERReq_SBI)
+	begin
+		State_DN <= State_DP;			-- Keep current state by default.
 
+		OutFifoWrite_SO <= '0';
+		OutFifoData_DO	<= (others => '0');
+
+		DVSAERAck_SBO	<= '1';			-- No acknowledge by default.
+		DVSAERReset_SBO <= '0';			-- Keep DVS in reset by default.
+
+		case State_DP is
+			when stIdle =>
+				-- Only exit idle state if DVS data producer is active.
+				if DVSRun_SI = '1' then
+					DVSAERReset_SBO <= '1';	 -- Keep DVS out of reset.
+
+					if DVSAERReq_SBI = '0' then
+						-- Got a request on the AER bus, let's get the data.
+						State_DN <= stWriteAddr;
+					end if;
+				end if;
+
+			when stWriteAddr =>
+				DVSAERReset_SBO <= '1';	 -- Keep DVS out of reset.
+
+				-- Get data and format it.
+				if DVSAERData_DI(9) = '0' then
+					-- This is an Y address.
+					OutFifoData_DO <= "0010000" & DVSAERData_DI(7 downto 0);
+				else
+					-- This is an X address. AER(8) holds the polarity.
+					if DVSAERData_DI(8) = '1' then
+						-- ON polarity.
+						OutFifoData_DO <= "0110000" & DVSAERData_DI(7 downto 0);
+					else
+						-- OFF polarity.
+						OutFifoData_DO <= "0100000" & DVSAERData_DI(7 downto 0);
+					end if;
+				end if;
+
+				OutFifoWrite_SO <= '1';
+
+				State_DN <= stAck;
+
+			when stAck =>
+				DVSAERReset_SBO <= '1';	 -- Keep DVS out of reset.
+
+				DVSAERAck_SBO <= '0';
+
+				if DVSAERReq_SBI = '1' then
+					State_DN <= stIdle;
+				end if;
+
+			when others => null;
+		end case;
+	end process p_memoryless;
+
+	-- Change state on clock edge (synchronous).
+	p_memoryzing : process (Clock_CI, Reset_RI)
+	begin
+		if Reset_RI = '1' then	-- asynchronous reset (active-high for FPGAs)
+			State_DP <= stIdle;
+		elsif rising_edge(Clock_CI) then
+			State_DP <= State_DN;
+		end if;
+	end process p_memoryzing;
 end Behavioral;
