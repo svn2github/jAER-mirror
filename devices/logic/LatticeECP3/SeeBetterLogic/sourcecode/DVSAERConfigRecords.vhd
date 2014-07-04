000404a3a87aa5bcc217c678bc199392efab7603
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-07-04 23:20:36 +0000

    SeeBetterLogic: make config parameter lenghts be automatically determined when used.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@4918 b7f4320f-462c-0410-a916-d9f35bb82d52

--- devices/logic/LatticeECP3/SeeBetterLogic/sourcecode/DVSAERConfigRecords.vhd
+++ devices/logic/LatticeECP3/SeeBetterLogic/sourcecode/DVSAERConfigRecords.vhd
@@ -9,9 +9,9 @@ package DVSAERConfigRecords is
 	end record tDVSAERConfig;
 
 	constant DVSAERCONFIG_MAX_PARAM_LENGTH : integer := 5;
-	constant DVSAERCONFIG_TOTAL_LENGTH	   : integer := 5;
+	constant DVSAERCONFIG_TOTAL_LENGTH	   : integer := tDVSAERConfig.ackDelay'length + tDVSAERConfig.ackExtension'length;
 
 	constant tDVSAERConfigDefault : tDVSAERConfig := (
-		ackDelay	 => to_unsigned(2, 5),
-		ackExtension => to_unsigned(1, 5));
+		ackDelay	 => to_unsigned(2, tDVSAERConfig.ackDelay'length),
+		ackExtension => to_unsigned(1, tDVSAERConfig.ackExtension'length));
 end package DVSAERConfigRecords;
