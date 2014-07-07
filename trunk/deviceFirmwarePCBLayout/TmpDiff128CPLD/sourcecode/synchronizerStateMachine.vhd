4b366690592e36fb9ce10b515535c2968afb4f05
hmontanaro <hmontanaro@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-07-07 10:39:51 +0000

    Added changes so that timestamp reset from master would be read in slaves all the time in DVS128
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@4927 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/deviceFirmwarePCBLayout/TmpDiff128CPLD/sourcecode/synchronizerStateMachine.vhd
+++ trunk/deviceFirmwarePCBLayout/TmpDiff128CPLD/sourcecode/synchronizerStateMachine.vhd
@@ -189,7 +189,7 @@ begin  -- Behavioral
           StatexDN <= stSlaveWaitEdge;
         end if;
         
-        if ConfigxSI='1'  then
+        if ConfigxSI='1' or CounterxDP > timeout then
           StatexDN   <= stIdle;
           CounterxDN <= (others => '0');
         end if;
