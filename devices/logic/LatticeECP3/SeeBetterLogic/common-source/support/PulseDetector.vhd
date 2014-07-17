2b7ec884680720b2109859836639c4c9fe29548d
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-07-17 16:29:45 +0000

    SeeBetterLogic: fix references to ceil/log2 math_real functions.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@5005 b7f4320f-462c-0410-a916-d9f35bb82d52

--- devices/logic/LatticeECP3/SeeBetterLogic/common-source/support/PulseDetector.vhd
+++ devices/logic/LatticeECP3/SeeBetterLogic/common-source/support/PulseDetector.vhd
@@ -1,8 +1,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use ieee.math_real."ceil";
-use ieee.math_real."log2";
+use ieee.math_real.ceil;
+use ieee.math_real.log2;
 
 entity PulseDetector is
 	generic (
--- devices/logic/LatticeECP3/SeeBetterLogic/common-source/support/PulseGenerator.vhd
+++ devices/logic/LatticeECP3/SeeBetterLogic/common-source/support/PulseGenerator.vhd
@@ -1,6 +1,6 @@
 library IEEE;
-use IEEE.MATH_REAL."ceil";
-use IEEE.MATH_REAL."log2";
+use IEEE.MATH_REAL.ceil;
+use IEEE.MATH_REAL.log2;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 
