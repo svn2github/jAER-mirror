eab794909b93156f3db7a665a423aa05278b9e2e
hongjie <hongjie@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-09-09 17:14:19 +0000

    ApproachSensitivity in FX3 folder simulation works
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@5174 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ devices/logic/LatticeECP3/SeeBetterLogic/FX3/ApproachSensitivity/syntmp/hdlorder.tcl
@@ -0,0 +1,1 @@
+project -fileorder "C:/lscc/diamond/3.2_x64/cae_library/synthesis/vhdl/ecp3.vhd" "E:/FX3/ApproachSensitivity/source/ApproachCell.vhd" "E:/FX3/ApproachSensitivity/source/ApproachCell_tb.vhd" 
