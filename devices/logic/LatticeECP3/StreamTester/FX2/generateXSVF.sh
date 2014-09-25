a9a503ed26337102e3303eac9f8e3396b3400b0d
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-09-25 19:25:58 +0000

    FX2 logic: add Windows script to generate XSVF from SVF automatically. Improve info message.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@5289 b7f4320f-462c-0410-a916-d9f35bb82d52

--- devices/logic/LatticeECP3/StreamTester/FX2/generateXSVF.sh
+++ devices/logic/LatticeECP3/StreamTester/FX2/generateXSVF.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+echo "Converting SVF file to XSVF format ..."
+
 ../../../svf2xsvf/svf2xsvf502 -extensions -w -i StreamTester_FX2/StreamTester_FX2.svf -o StreamTester_FX2/StreamTester_FX2.xsvf
