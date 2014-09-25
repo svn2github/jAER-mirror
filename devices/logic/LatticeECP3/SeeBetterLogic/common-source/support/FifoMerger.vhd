24ecf1f0fdd0d9e59ff9e8ba1465479dcc50e2fb
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-09-25 13:35:23 +0000

    SeeBetterLogic FifoMerger: Fix missing others clause in case statement error (in sequential part). Remove unused signals. Rename signals to match the naming convention in use. Encode state as onehot for performance. Format file.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@5283 b7f4320f-462c-0410-a916-d9f35bb82d52

--- devices/logic/LatticeECP3/SeeBetterLogic/common-source/support/FifoMerger.vhd
+++ devices/logic/LatticeECP3/SeeBetterLogic/common-source/support/FifoMerger.vhd
@@ -10,16 +10,16 @@ entity FifoMerger is
 	generic(
 		FIFO_WIDTH : integer);
 	port(
-		Clock_CI           : in  std_logic;
-		Reset_RI           : in  std_logic;
+		Clock_CI          : in  std_logic;
+		Reset_RI          : in  std_logic;
 
-		FifoIn1Control_SI   : in  tFromFifoReadSide;
-		FifoIn1Control_SO   : out tToFifoReadSide;
-		FifoIn1Data_DI      : in  std_logic_vector(FIFO_WIDTH - 1 downto 0);
-		
-		FifoIn2Control_SI   : in  tFromFifoReadSide;
-		FifoIn2Control_SO   : out tToFifoReadSide;
-		FifoIn2Data_DI      : in  std_logic_vector(FIFO_WIDTH - 1 downto 0);
+		FifoIn1Control_SI : in  tFromFifoReadSide;
+		FifoIn1Control_SO : out tToFifoReadSide;
+		FifoIn1Data_DI    : in  std_logic_vector(FIFO_WIDTH - 1 downto 0);
+
+		FifoIn2Control_SI : in  tFromFifoReadSide;
+		FifoIn2Control_SO : out tToFifoReadSide;
+		FifoIn2Data_DI    : in  std_logic_vector(FIFO_WIDTH - 1 downto 0);
 
 		FifoOutControl_SI : in  tFromFifoWriteSide;
 		FifoOutControl_SO : out tToFifoWriteSide;
@@ -27,62 +27,69 @@ entity FifoMerger is
 end entity FifoMerger;
 
 architecture Behavioral of FifoMerger is
-	type tSMmerger is (idle, FIFO1, FIFO2);
-	signal cs, ns: tSMmerger;
-	signal Arbiter_priority  : integer range 1 to 2;
-	
-	signal FifoIn1NotEmpty_S : std_logic;
-	signal FifoIn2NotEmpty_S : std_logic;
+	attribute syn_enum_encoding : string;
 
-	signal WriteDelayReg_S : std_logic;
+	type tState is (idle, FIFO1, FIFO2);
+	attribute syn_enum_encoding of tState : type is "onehot";
+
+	-- present and next state
+	signal State_DP, State_DN : tState;
+
+	signal ArbiterPriority_S : integer range 1 to 2;
 
 	signal DataFifoOutReg_D : std_logic_vector(FIFO_WIDTH - 1 downto 0);
-	
 begin
-	SM_comb: process(cs, FifoIn1Control_SI, FifoIn2Control_SI, FifoOutControl_SI, Arbiter_priority)
+	SM_comb : process(State_DP, FifoIn1Control_SI, FifoIn2Control_SI, FifoOutControl_SI, ArbiterPriority_S)
 	begin
-	    case cs is
-		    when idle =>
-			    if (FifoIn1Control_SI.Empty_S='0' and FifoIn2Control_SI.Empty_S='1' and FifoOutControl_SI.Full_S='0') then
-				   ns <= FIFO1;
-			    elsif (FifoIn1Control_SI.Empty_S='1' and FifoIn2Control_SI.Empty_S='0' and FifoOutControl_SI.Full_S='0') then
-				   ns <= FIFO2;
-			    elsif (FifoIn1Control_SI.Empty_S='0' and FifoIn2Control_SI.Empty_S='0' and FifoOutControl_SI.Full_S='0') then
-				   if (Arbiter_priority = 1) then 
-				        ns <= FIFO1;
-				   else ns <= FIFO2;
-				   end if;				else
-				   ns <= idle;
+		case State_DP is
+			when idle =>
+				if (FifoIn1Control_SI.Empty_S = '0' and FifoIn2Control_SI.Empty_S = '1' and FifoOutControl_SI.Full_S = '0') then
+					State_DN <= FIFO1;
+				elsif (FifoIn1Control_SI.Empty_S = '1' and FifoIn2Control_SI.Empty_S = '0' and FifoOutControl_SI.Full_S = '0') then
+					State_DN <= FIFO2;
+				elsif (FifoIn1Control_SI.Empty_S = '0' and FifoIn2Control_SI.Empty_S = '0' and FifoOutControl_SI.Full_S = '0') then
+					if (ArbiterPriority_S = 1) then
+						State_DN <= FIFO1;
+					else
+						State_DN <= FIFO2;
+					end if;
+				else
+					State_DN <= idle;
 				end if;
-			when others => 
-			    ns <= idle;
+
+			when others =>
+				State_DN <= idle;
 		end case;
 	end process;
-   
+
 	SM_seq : process(Clock_CI, Reset_RI) is
 	begin
 		if Reset_RI = '1' then          -- asynchronous reset (active high)
 			FifoOutControl_SO.Write_S <= '0';
 			FifoIn1Control_SO.Read_S  <= '0';
 			FifoIn2Control_SO.Read_S  <= '0';
-			DataFifoOutReg_D           <= (others => '0');
-			Arbiter_priority		   <= 1;
+			DataFifoOutReg_D          <= (others => '0');
+			ArbiterPriority_S         <= 1;
 		elsif rising_edge(Clock_CI) then -- rising clock edge
-		    cs <= ns;
+			State_DP                  <= State_DN;
 			FifoOutControl_SO.Write_S <= '0';
 			FifoIn1Control_SO.Read_S  <= '0';
 			FifoIn2Control_SO.Read_S  <= '0';
-			case (cs) is
-			    when FIFO1 => 
-				    Arbiter_priority          <= 2;
+
+			case (State_DP) is
+				when FIFO1 =>
+					ArbiterPriority_S         <= 2;
 					FifoOutcontrol_SO.Write_S <= '1';
 					FifoIn1Control_SO.Read_S  <= '1';
 					DataFifoOutReg_D          <= FifoIn1Data_DI;
-				when FIFO2 => 
-				    Arbiter_priority <= 1;
+
+				when FIFO2 =>
+					ArbiterPriority_S         <= 1;
 					FifoOutcontrol_SO.Write_S <= '1';
 					FifoIn2Control_SO.Read_S  <= '1';
 					DataFifoOutReg_D          <= FifoIn2Data_DI;
+
+				when others => null;
 			end case;
 		end if;
 	end process;
