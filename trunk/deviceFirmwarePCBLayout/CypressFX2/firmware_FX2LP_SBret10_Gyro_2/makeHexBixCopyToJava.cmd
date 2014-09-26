92805df2b15c2bc3d9cc56f950717a4d9021cc4c
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-09-26 12:43:48 +0000

    SBRet10 FX2 fw (old): ensure memory spaces are correctly set. Fix paths to files to conform to standard Cypress installation directory. Fix generating of IIC/BIX files.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@5294 b7f4320f-462c-0410-a916-d9f35bb82d52

--- trunk/deviceFirmwarePCBLayout/CypressFX2/firmware_FX2LP_SBret10_Gyro_2/makeHexBixCopyToJava.cmd
+++ trunk/deviceFirmwarePCBLayout/CypressFX2/firmware_FX2LP_SBret10_Gyro_2/makeHexBixCopyToJava.cmd
@@ -2,8 +2,8 @@
 rem ******************** make binary I2C download file from hex project output
 rem we are still using .bix file format because the download definitely works with it from jAER
 @echo on
-hex2bix -i -M 14592 -F 0xC2 -o SBret10.iic SBret10.hex
-hex2bix -b -R -M 14592 -o SBret10.bix SBret10.hex
+Hex2bix.exe -i -V 0x152A -P 0x840D -M 0x4000 -F 0xC2 -C 0x01 -o SBret10.iic SBret10.hex
+Hex2bix.exe -b -V 0x152A -P 0x840D -M 0x4000 -o SBret10.bix SBret10.hex
 
 @echo on
 
