7f712daa2552a5d312e42fc557e0a21435478c38
diederikmoeys <diederikmoeys@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-10-01 13:36:04 +0000

    OMC with merger
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@5336 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ devices/logic/LatticeECP3/SeeBetterLogic/FX3_OMC/private-source/Settings.vhd
@@ -0,0 +1,34 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+package Settings is
+	constant DEVICE_FAMILY : string := "ECP3";
+
+	constant USB_CLOCK_FREQ         : integer := 80; -- 80 or 100 are viable settings, depending on FX3 and routing.
+	constant USB_FIFO_WIDTH         : integer := 16;
+	constant USB_EARLY_PACKET_MS    : integer := 1; -- send a packet each X milliseconds
+	constant USB_BURST_WRITE_LENGTH : integer := 8;
+
+	constant LOGIC_CLOCK_FREQ       : integer := 60; -- PLL can generate between 5 and 500 MHz here.
+
+	constant AER_BUS_WIDTH          : integer := 10;
+	constant ADC_BUS_WIDTH          : integer := 10;
+	constant MISC_OBT_AER_BUS_WIDTH : integer := 18;
+
+
+	constant USBLOGIC_FIFO_SIZE                 : integer := 32;
+	constant USBLOGIC_FIFO_ALMOST_EMPTY_SIZE    : integer := USB_BURST_WRITE_LENGTH;
+	constant USBLOGIC_FIFO_ALMOST_FULL_SIZE     : integer := 2;
+	constant DVSAER_FIFO_SIZE                   : integer := 16;
+	constant DVSAER_FIFO_ALMOST_EMPTY_SIZE      : integer := 4;
+	constant DVSAER_FIFO_ALMOST_FULL_SIZE       : integer := 2;
+	constant APSADC_FIFO_SIZE                   : integer := 128;
+	constant APSADC_FIFO_ALMOST_EMPTY_SIZE      : integer := 8;
+	constant APSADC_FIFO_ALMOST_FULL_SIZE       : integer := 8;
+	constant IMU_FIFO_SIZE                      : integer := 14; -- two samples (2x7)
+	constant IMU_FIFO_ALMOST_EMPTY_SIZE         : integer := 7; -- one sample (1x7)
+	constant IMU_FIFO_ALMOST_FULL_SIZE          : integer := 7; -- one sample (1x7)
+	constant EXT_TRIGGER_FIFO_SIZE              : integer := 4;
+	constant EXT_TRIGGER_FIFO_ALMOST_EMPTY_SIZE : integer := 1;
+	constant EXT_TRIGGER_FIFO_ALMOST_FULL_SIZE  : integer := 1;
+end Settings;
