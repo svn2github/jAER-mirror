de3135992fd51de762f72d24457ed155020153b3
hongjie <hongjie@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-10-02 14:35:38 +0000

    most updated AC with working AC file error free top level and config records,next step SPI configuration.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@5349 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ devices/logic/LatticeECP3/SeeBetterLogic/FX3ApproachSensitivity/ApproachSensitivity0924/source/ApproachCellMain.vhd
@@ -0,0 +1,150 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.ACConfigRecords.all;
+
+
+entity ApproachCellMain is
+ 
+
+	--generic (DecayCounter_Size : Integer := 32;
+			 --TimeCounter_Size : Integer := 32;
+			 --UpdateUnit:  signed (7 downto 0);
+			 --IFThreshold: signed(63 downto 0)
+			 --);
+	
+    port(
+		Clock_CI                 : in  std_logic;
+		Reset_RI                 : in  std_logic;
+		
+		SurSupEnable_I : in std_logic; 
+		CAVIAR_req    : in std_logic;
+		WS_ack        : in std_logic;
+		CAVIAR_data   : in  std_logic_vector(16 downto 0);
+		
+		AC_Ack_O : 		out std_logic;
+		AC_Req_O : 		out  std_logic;
+		AC_Fire_O :	    out std_logic_vector (5 downto 0);
+		ACConfig_DI:    in  tACConfig
+		);
+		
+end entity ApproachCellMain;
+
+
+architecture Behavioral of ApproachCellMain is
+
+	--attribute syn_enum_encoding : string
+
+	--type state is (stIdle,);
+	
+	--attribute syn_enum_encoding of state : type is "onehot";
+	type tDVSEventR is array ( 7 downto 0, 7 downto 0) of std_logic;
+	
+	--signal State_DP, State_DN : state;	
+	
+	----Enable Counter? when----
+	signal EnableCounter: std_logic :='1';
+	
+	----2 Counter Output----
+	signal DecayEnable : std_logic;
+	signal ACConfigReg_D: tACConfig;
+	signal CounterOut: unsigned(ACConfigReg_D.TimeCounter_Size - 1 downto 0);
+	 
+	----Decoded DVS Address-------
+	--signal DVSEventReq,  AC_Ack : tDVSEventR;  --3bits *3bits---
+	signal EventPolarity : std_logic;  ---1bit----
+	signal EventYAddr, EventXAddr: std_logic_vector( 4 downto 0);  ---3bits *3bits----
+	constant  DataLimit: unsigned (ACConfigReg_D.TimeCounter_Size -1 downto 0) := (others=> '1');
+	signal AC_Fire,  AC_Ack, DVSEventReq : tDVSEventR;
+	
+
+	
+			
+begin
+	
+	DecayCounter : entity work.ContinuousCounter
+	
+		generic map(
+			SIZE => ACConfigReg_D.DecayCounter_Size)
+		port map(
+			Clock_CI     => Clock_CI,
+			Reset_RI     => Reset_RI,
+			Clear_SI     => '0',
+			Enable_SI    => EnableCounter,-----
+			DataLimit_DI => DataLimit,
+			Overflow_SO  => DecayEnable,  
+			Data_DO      => open );
+			
+
+	EventTimestampCounter : entity work.ContinuousCounter
+	
+		generic map(
+			SIZE => ACConfigReg_D.TimeCounter_Size)
+		port map(
+			Clock_CI     => Clock_CI,
+			Reset_RI     => Reset_RI,
+			Clear_SI     => '0',
+			Enable_SI    => EnableCounter,-----
+			DataLimit_DI => DataLimit,
+			Overflow_SO  => open,  
+			Data_DO      => CounterOut);
+		
+
+	ApproachCells : for k in 7 downto 0 generate
+			ApproacCellMember : for m in 7 downto 0 generate
+						AC: entity work.AC
+						
+						   generic map (
+								CounterSize => ACConfigReg_D.TimeCounter_Size,
+								UpdateUnit => ACConfigReg_D.UpdateUnit,
+								IFThreshold => ACConfigReg_D.IFThreshold )
+								
+							port map 
+							( 
+								Clock_CI     => Clock_CI,
+								Reset_RI     => Reset_RI,
+								DVSEvent_I     => DVSEventReq(k,m),
+								EventXAddr_I    => EventXAddr,
+								EventYAddr_I => EventYAddr,
+								EventPolarity_I  => EventPolarity, 
+								DecayEnable_I =>  DecayEnable,
+								CounterOut_I  => CounterOut,
+								surroundSuppressionEnabled_I  => SurSupEnable_I,		
+								AC_Fire_O    => AC_Fire(k,m),
+								AC_Ack_O     => AC_Ack(k,m));
+		   end generate;
+	   end generate;
+	   
+	p_memoryzing : process(Clock_CI, Reset_RI)
+	begin
+		--if Reset_RI = '1' then          -- asynchronous reset (active-high for FPGAs)
+			--State_DP <= stIdle;	  
+			
+		if rising_edge(Clock_CI) then
+			EventXAddr <= CAVIAR_data ( 5 downto 1 );
+			EventYAddr <= CAVIAR_data ( 13 downto 9 );
+			
+			
+			for k in 0 to 7 loop
+				for m in 0 to 7 loop
+				
+					if k= unsigned (CAVIAR_data (16 downto 14)) and m= unsigned (CAVIAR_data (8 downto 6)) then 
+					   DVSEventReq(k,m) <= CAVIAR_req; 
+					end if;
+					
+					if  AC_Ack(k,m) = '1' then
+						AC_Ack_O <= '1';
+						AC_Fire_O (5 downto 3) <= std_logic_vector (to_unsigned (m, 3)); 
+						AC_Fire_O (2 downto 0) <= std_logic_vector (to_unsigned (k, 3));
+					end if;
+							
+				end loop;
+			end loop;
+			ACConfigReg_D <= ACConfig_DI;
+		end if;
+	end process;
+end Behavioral;
+
+
+
+			
---
+++ devices/logic/LatticeECP3/SeeBetterLogic/FX3ApproachSensitivity/ApproachSensitivity0924/source/DVSAERSPIConfig.vhd
@@ -0,0 +1,70 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.DVSAERConfigRecords.all;
+
+entity DVSAERSPIConfig is
+	port(
+		Clock_CI                   : in  std_logic;
+		Reset_RI                   : in  std_logic;
+		DVSAERConfig_DO            : out tDVSAERConfig;
+
+		-- SPI configuration inputs and outputs.
+		ConfigModuleAddress_DI     : in  unsigned(6 downto 0);
+		ConfigParamAddress_DI      : in  unsigned(7 downto 0);
+		ConfigParamInput_DI        : in  std_logic_vector(31 downto 0);
+		ConfigLatchInput_SI        : in  std_logic;
+		DVSAERConfigParamOutput_DO : out std_logic_vector(31 downto 0));
+end entity DVSAERSPIConfig;
+
+architecture Behavioral of DVSAERSPIConfig is
+	signal LatchDVSAERReg_S                       : std_logic;
+	signal DVSAERInput_DP, DVSAERInput_DN         : std_logic_vector(31 downto 0);
+	signal DVSAEROutput_DP, DVSAEROutput_DN       : std_logic_vector(31 downto 0);
+	signal DVSAERConfigReg_DP, DVSAERConfigReg_DN : tDVSAERConfig;
+begin
+	DVSAERConfig_DO            <= DVSAERConfigReg_DP;
+	DVSAERConfigParamOutput_DO <= DVSAEROutput_DP;
+
+	LatchDVSAERReg_S <= '1' when ConfigModuleAddress_DI = DVSAERCONFIG_MODULE_ADDRESS else '0';
+
+	dvsaerIO : process(ConfigParamAddress_DI, ConfigParamInput_DI, DVSAERInput_DP, DVSAERConfigReg_DP)
+	begin
+		DVSAERConfigReg_DN <= DVSAERConfigReg_DP;
+		DVSAERInput_DN     <= ConfigParamInput_DI;
+		DVSAEROutput_DN    <= (others => '0');
+
+		case ConfigParamAddress_DI is
+			when DVSAERCONFIG_PARAM_ADDRESSES.Run_S =>
+				DVSAERConfigReg_DN.Run_S <= DVSAERInput_DP(0);
+				DVSAEROutput_DN(0)       <= DVSAERConfigReg_DP.Run_S;
+
+			when DVSAERCONFIG_PARAM_ADDRESSES.AckDelay_D =>
+				DVSAERConfigReg_DN.AckDelay_D                                 <= unsigned(DVSAERInput_DP(tDVSAERConfig.AckDelay_D'length - 1 downto 0));
+				DVSAEROutput_DN(tDVSAERConfig.AckDelay_D'length - 1 downto 0) <= std_logic_vector(DVSAERConfigReg_DP.AckDelay_D);
+
+			when DVSAERCONFIG_PARAM_ADDRESSES.AckExtension_D =>
+				DVSAERConfigReg_DN.AckExtension_D                                 <= unsigned(DVSAERInput_DP(tDVSAERConfig.AckExtension_D'length - 1 downto 0));
+				DVSAEROutput_DN(tDVSAERConfig.AckExtension_D'length - 1 downto 0) <= std_logic_vector(DVSAERConfigReg_DP.AckExtension_D);
+
+			when others => null;
+		end case;
+	end process dvsaerIO;
+
+	dvsaerUpdate : process(Clock_CI, Reset_RI) is
+	begin
+		if Reset_RI = '1' then          -- asynchronous reset (active high)
+			DVSAERInput_DP  <= (others => '0');
+			DVSAEROutput_DP <= (others => '0');
+
+			DVSAERConfigReg_DP <= tDVSAERConfigDefault;
+		elsif rising_edge(Clock_CI) then -- rising clock edge
+			DVSAERInput_DP  <= DVSAERInput_DN;
+			DVSAEROutput_DP <= DVSAEROutput_DN;
+
+			if LatchDVSAERReg_S = '1' and ConfigLatchInput_SI = '1' then
+				DVSAERConfigReg_DP <= DVSAERConfigReg_DN;
+			end if;
+		end if;
+	end process dvsaerUpdate;
+end architecture Behavioral;
