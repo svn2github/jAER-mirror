b3abef8704a6d857341004e646e394d84057287a
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-10-07 14:21:59 +0000

    SBret10 logic: remove temporary project files from Lattice.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@5398 b7f4320f-462c-0410-a916-d9f35bb82d52

--- devices/logic/LatticeMachXO/SBret10_2_IMU/SBret10/synlog.tcl
+++
@@ -1,5 +0,0 @@
-source "C:/Users/Haza/AppData/Local/Synplicity/scm_perforce.tcl"
-history clear
-run_tcl -fg C:/Users/Haza/Documents/jAER/deviceFirmwarePCBLayout/LatticeMachXO/SBret10_2_IMU/SBret10/launch_synplify.tcl
-project -run  -bg 
-project -close C:/Users/Haza/Documents/jAER/deviceFirmwarePCBLayout/LatticeMachXO/SBret10_2_IMU/SBret10/SBret10_syn.prj
