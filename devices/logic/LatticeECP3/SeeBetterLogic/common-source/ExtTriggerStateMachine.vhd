f6a607e3e8c29c3cfd9b2c4084b85382a4c641f1
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-10-30 12:48:51 +0000

    ExtTrigger logic: fix config register name. Add direct redirection of input trigger to output port if generator is disabled.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@5549 b7f4320f-462c-0410-a916-d9f35bb82d52

--- devices/logic/LatticeECP3/SeeBetterLogic/common-source/ExtTriggerStateMachine.vhd
+++ devices/logic/LatticeECP3/SeeBetterLogic/common-source/ExtTriggerStateMachine.vhd
@@ -54,9 +54,9 @@ architecture Behavioral of ExtTriggerStateMachine is
 	signal PulseDetected_S       : std_logic;
 
 	-- Register configuration inputs.
-	signal ExtTriggerConfig_D : tExtTriggerConfig;
+	signal ExtTriggerConfigReg_D : tExtTriggerConfig;
 begin
-	extTriggerDetectorLogic : process(State_DP, OutFifoControl_SI, ExtTriggerConfig_D, FallingEdgeDetected_S, PulseDetected_S, RisingEdgeDetected_S)
+	extTriggerDetectorLogic : process(State_DP, OutFifoControl_SI, ExtTriggerConfigReg_D, FallingEdgeDetected_S, PulseDetected_S, RisingEdgeDetected_S)
 	begin
 		State_DN <= State_DP;           -- Keep current state by default.
 
@@ -66,15 +66,15 @@ begin
 		case State_DP is
 			when stIdle =>
 				-- Only exit idle state if External Trigger data producer is active and FIFO has space.
-				if ExtTriggerConfig_D.RunDetector_S = '1' and OutFifoControl_SI.Full_S = '0' then
+				if ExtTriggerConfigReg_D.RunDetector_S = '1' and OutFifoControl_SI.Full_S = '0' then
 					-- TODO: verify what happens if/when multiple fire together.
-					if ExtTriggerConfig_D.DetectRisingEdges_S = '1' and RisingEdgeDetected_S = '1' then
+					if ExtTriggerConfigReg_D.DetectRisingEdges_S = '1' and RisingEdgeDetected_S = '1' then
 						State_DN <= stWriteRisingEdgeEvent;
 					end if;
-					if ExtTriggerConfig_D.DetectFallingEdges_S = '1' and FallingEdgeDetected_S = '1' then
+					if ExtTriggerConfigReg_D.DetectFallingEdges_S = '1' and FallingEdgeDetected_S = '1' then
 						State_DN <= stWriteFallingEdgeEvent;
 					end if;
-					if ExtTriggerConfig_D.DetectPulses_S = '1' and PulseDetected_S = '1' then
+					if ExtTriggerConfigReg_D.DetectPulses_S = '1' and PulseDetected_S = '1' then
 						State_DN <= stWritePulseEvent;
 					end if;
 				end if;
@@ -104,11 +104,11 @@ begin
 		if Reset_RI = '1' then          -- asynchronous reset (active-high for FPGAs)
 			State_DP <= stIdle;
 
-			ExtTriggerConfig_D <= tExtTriggerConfigDefault;
+			ExtTriggerConfigReg_D <= tExtTriggerConfigDefault;
 		elsif rising_edge(Clock_CI) then
 			State_DP <= State_DN;
 
-			ExtTriggerConfig_D <= ExtTriggerConfig_DI;
+			ExtTriggerConfigReg_D <= ExtTriggerConfig_DI;
 		end if;
 	end process registerUpdate;
 
@@ -123,7 +123,7 @@ begin
 			FallingEdgeDetected_SO => FallingEdgeDetected_S);
 
 	-- Calculate values in cycles for pulse detector and generator, by multiplying time-slice number by cycles in that time-slice.
-	DetectPulseLength_D <= ExtTriggerConfig_D.DetectPulseLength_D * to_unsigned(TRIGGER_TIME_CYCLES, TRIGGER_TIME_CYCLES_SIZE);
+	DetectPulseLength_D <= ExtTriggerConfigReg_D.DetectPulseLength_D * to_unsigned(TRIGGER_TIME_CYCLES, TRIGGER_TIME_CYCLES_SIZE);
 
 	extTriggerPulseDetector : entity work.PulseDetector
 		generic map(
@@ -131,7 +131,7 @@ begin
 		port map(
 			Clock_CI         => Clock_CI,
 			Reset_RI         => Reset_RI,
-			PulsePolarity_SI => ExtTriggerConfig_D.DetectPulsePolarity_S,
+			PulsePolarity_SI => ExtTriggerConfigReg_D.DetectPulsePolarity_S,
 			PulseLength_DI   => DetectPulseLength_D,
 			InputSignal_SI   => ExtTriggerSignal_SI,
 			PulseDetected_SO => PulseDetected_S);
@@ -139,6 +139,7 @@ begin
 	generator : if ENABLE_GENERATOR = true generate
 		-- Generator signal.
 		signal GeneratedPulse_S      : std_logic;
+		signal ExtTriggerOut_S       : std_logic;
 		signal ExtTriggerSignalOut_S : std_logic;
 
 		-- Generator configuration signals.
@@ -146,8 +147,8 @@ begin
 		signal GeneratePulseLength_D   : unsigned(TRIGGER_CYCLES_SIZE - 1 downto 0);
 	begin
 		-- Calculate values in cycles for pulse detector and generator, by multiplying time-slice number by cycles in that time-slice.
-		GeneratePulseInterval_D <= ExtTriggerConfig_D.GeneratePulseInterval_D * to_unsigned(TRIGGER_TIME_CYCLES, TRIGGER_TIME_CYCLES_SIZE);
-		GeneratePulseLength_D   <= ExtTriggerConfig_D.GeneratePulseLength_D * to_unsigned(TRIGGER_TIME_CYCLES, TRIGGER_TIME_CYCLES_SIZE);
+		GeneratePulseInterval_D <= ExtTriggerConfigReg_D.GeneratePulseInterval_D * to_unsigned(TRIGGER_TIME_CYCLES, TRIGGER_TIME_CYCLES_SIZE);
+		GeneratePulseLength_D   <= ExtTriggerConfigReg_D.GeneratePulseLength_D * to_unsigned(TRIGGER_TIME_CYCLES, TRIGGER_TIME_CYCLES_SIZE);
 
 		extTriggerPulseGenerator : entity work.PulseGenerator
 			generic map(
@@ -156,13 +157,14 @@ begin
 			port map(
 				Clock_CI         => Clock_CI,
 				Reset_RI         => Reset_RI,
-				PulsePolarity_SI => ExtTriggerConfig_D.GeneratePulsePolarity_S,
+				PulsePolarity_SI => ExtTriggerConfigReg_D.GeneratePulsePolarity_S,
 				PulseInterval_DI => GeneratePulseInterval_D,
 				PulseLength_DI   => GeneratePulseLength_D,
-				Zero_SI          => not ExtTriggerConfig_D.RunGenerator_S,
+				Zero_SI          => not ExtTriggerConfigReg_D.RunGenerator_S or ExtTriggerConfigReg_D.GenerateUseCustomSignal_S,
 				PulseOut_SO      => GeneratedPulse_S);
 
-		ExtTriggerSignalOut_S <= CustomTriggerSignal_SI when (ExtTriggerConfig_D.RunGenerator_S = '1' and ExtTriggerConfig_D.GenerateUseCustomSignal_S = '1') else GeneratedPulse_S;
+		ExtTriggerOut_S       <= CustomTriggerSignal_SI when (ExtTriggerConfigReg_D.GenerateUseCustomSignal_S = '1') else GeneratedPulse_S;
+		ExtTriggerSignalOut_S <= ExtTriggerOut_S when (ExtTriggerConfigReg_D.RunGenerator_S = '1') else ExtTriggerSignal_SI;
 
 		-- Register output to meet timing specifications.
 		extTriggerSignalOutBuffer : entity work.SimpleRegister
@@ -178,7 +180,7 @@ begin
 	end generate generator;
 
 	generatorDisabled : if ENABLE_GENERATOR = false generate
-		-- Output overflow (constant zero).
-		ExtTriggerSignal_SO <= '0';
+		-- If generator disabled, just redirect input to output.
+		ExtTriggerSignal_SO <= ExtTriggerSignal_SI;
 	end generate generatorDisabled;
 end architecture Behavioral;
