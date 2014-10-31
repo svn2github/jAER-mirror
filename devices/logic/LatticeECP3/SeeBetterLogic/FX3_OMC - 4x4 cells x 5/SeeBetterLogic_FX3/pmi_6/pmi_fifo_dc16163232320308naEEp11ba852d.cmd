e76a63b55a2681d1b14e2bd8e317a4332c207f6d
diederikmoeys <diederikmoeys@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-10-31 10:34:27 +0000

    files
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@5558 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ devices/logic/LatticeECP3/SeeBetterLogic/FX3_OMC - 4x4 cells x 5/SeeBetterLogic_FX3/pmi_6/pmi_fifo_dc16163232320308naEEp11ba852d.cmd
@@ -0,0 +1,7 @@
+
+
+
+
+
+
+"C:/lscc/diamond/3.2_x64/ispfpga/bin/nt64/scuba" -w -bus_exp 7 -bb -arch ep5c00  -type ebfifo -depth 32 -width 16 -pf 30 -pe 8  -n pmi_fifo_dc16163232320308naEEp11ba852d -pmi
