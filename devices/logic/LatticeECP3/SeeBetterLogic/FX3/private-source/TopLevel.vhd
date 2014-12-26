c22e4e56d825294fad113c55620d40e7c977404f
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2014-12-26 16:44:34 +0000

    SBL: commit up-to-date FX3 binary. Disable BA filter for FX3, timing can't be met. Enable register retiming in MAP phase too.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@5800 b7f4320f-462c-0410-a916-d9f35bb82d52

--- devices/logic/LatticeECP3/SeeBetterLogic/FX3/private-source/TopLevel.vhd
+++ devices/logic/LatticeECP3/SeeBetterLogic/FX3/private-source/TopLevel.vhd
@@ -342,8 +342,7 @@ begin
 	dvsAerSM : entity work.DVSAERStateMachine
 		generic map(
 			FLIP_COLUMN_ADDRESS    => true,
-			ENABLE_PIXEL_FILTERING => true,
-			ENABLE_BA_FILTERING    => true)
+			ENABLE_PIXEL_FILTERING => true)
 		port map(
 			Clock_CI          => LogicClock_C,
 			Reset_RI          => LogicReset_R,
@@ -358,8 +357,7 @@ begin
 
 	dvsaerSPIConfig : entity work.DVSAERSPIConfig
 		generic map(
-			ENABLE_PIXEL_FILTERING => true,
-			ENABLE_BA_FILTERING    => true)
+			ENABLE_PIXEL_FILTERING => true)
 		port map(
 			Clock_CI                   => LogicClock_C,
 			Reset_RI                   => LogicReset_R,
