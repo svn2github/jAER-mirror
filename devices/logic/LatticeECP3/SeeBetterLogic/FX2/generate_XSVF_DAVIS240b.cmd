930afd105fd26a347c5fb2d51c5f6663405c96e8
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2015-01-19 12:23:19 +0000

    SBL: split SVF/XSVF generation files, given the different binary directories.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@5898 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ devices/logic/LatticeECP3/SeeBetterLogic/FX2/generate_XSVF_DAVIS240b.cmd
@@ -0,0 +1,3 @@
+@echo Converting SVF file to XSVF format ...
+
+..\..\..\svf2xsvf\svf2xsvf502.exe -extensions -w -i SeeBetterLogic_FX2\SeeBetterLogic_FX2.svf -o SeeBetterLogic_FX2\SeeBetterLogic_FX2.xsvf
---
+++ devices/logic/LatticeECP3/SeeBetterLogic/FX2/generate_XSVF_DAVIS240b.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Converting SVF file to XSVF format ..."
+
+../../../svf2xsvf/svf2xsvf502 -extensions -w -i SeeBetterLogic_FX2/SeeBetterLogic_FX2.svf -o SeeBetterLogic_FX2/SeeBetterLogic_FX2.xsvf
