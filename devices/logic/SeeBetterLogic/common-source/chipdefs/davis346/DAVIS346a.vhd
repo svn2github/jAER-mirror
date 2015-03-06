edeaa39b8ab3adf3374defbdbd844086d6a6a086
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2015-03-06 15:16:48 +0000

    SBL: update DAVIS346 definitions, according to Chenghan there are 260 pixels and AER bus is always 11 bits wide.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@6156 b7f4320f-462c-0410-a916-d9f35bb82d52

--- devices/logic/SeeBetterLogic/common-source/chipdefs/davis346/DAVIS346a.vhd
+++ devices/logic/SeeBetterLogic/common-source/chipdefs/davis346/DAVIS346a.vhd
@@ -9,11 +9,11 @@ package DAVIS346a is
 	constant CHIP_HAS_INTEGRATED_ADC : std_logic := '1';
 
 	constant CHIP_APS_SIZE_ROWS    : unsigned(8 downto 0) := to_unsigned(346, 9);
-	constant CHIP_APS_SIZE_COLUMNS : unsigned(7 downto 0) := to_unsigned(240, 8);
+	constant CHIP_APS_SIZE_COLUMNS : unsigned(8 downto 0) := to_unsigned(260, 9);
 
 	constant CHIP_DVS_SIZE_ROWS    : unsigned(8 downto 0) := to_unsigned(346, 9);
-	constant CHIP_DVS_SIZE_COLUMNS : unsigned(7 downto 0) := to_unsigned(240, 8);
+	constant CHIP_DVS_SIZE_COLUMNS : unsigned(8 downto 0) := to_unsigned(260, 9);
 
-	constant DVS_AER_BUS_WIDTH : integer := 10;
+	constant DVS_AER_BUS_WIDTH : integer := 11;
 	constant APS_ADC_BUS_WIDTH : integer := 10;
 end package DAVIS346a;
