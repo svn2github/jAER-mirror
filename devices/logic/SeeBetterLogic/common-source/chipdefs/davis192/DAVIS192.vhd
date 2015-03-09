91b168e57542ba112a43f17ea94c90e3afbe74a0
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2015-03-09 13:38:01 +0000

    SBL: fix DAVIS192 chip ID to be 8.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@6166 b7f4320f-462c-0410-a916-d9f35bb82d52

--- devices/logic/SeeBetterLogic/common-source/chipdefs/davis192/DAVIS192.vhd
+++ devices/logic/SeeBetterLogic/common-source/chipdefs/davis192/DAVIS192.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 package DAVIS128 is
-	constant CHIP_IDENTIFIER : unsigned(3 downto 0) := to_unsigned(3, 4);
+	constant CHIP_IDENTIFIER : unsigned(3 downto 0) := to_unsigned(8, 4);
 
 	constant CHIP_HAS_GLOBAL_SHUTTER : std_logic := '1';
 	constant CHIP_HAS_INTEGRATED_ADC : std_logic := '1';
