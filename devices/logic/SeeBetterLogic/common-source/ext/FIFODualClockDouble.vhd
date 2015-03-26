1bc72f9871420bb75308949ab340c3c305e95d68
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2015-03-26 18:43:33 +0000

    SBL: fix new FIFO type for ECP3.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@6293 b7f4320f-462c-0410-a916-d9f35bb82d52

--- devices/logic/SeeBetterLogic/common-source/ext/FIFODualClockDouble.vhd
+++ devices/logic/SeeBetterLogic/common-source/ext/FIFODualClockDouble.vhd
@@ -44,33 +44,57 @@ begin
 	-- This is a more portable variation than what you'd get with the other tools,
 	-- but slightly less configurable. It has everything we need though, and allows
 	-- for easy switching between underlying hardware implementations and tuning.
-	fifoDualClock : component work.pmi_components.pmi_fifo_dc
-		generic map(
-			pmi_data_width_w      => DATA_WIDTH_IN,
-			pmi_data_width_r      => DATA_WIDTH,
-			pmi_data_depth_w      => DATA_DEPTH_IN,
-			pmi_data_depth_r      => DATA_DEPTH,
-			pmi_full_flag         => DATA_DEPTH_IN,
-			pmi_empty_flag        => 0,
-			pmi_almost_full_flag  => DATA_DEPTH_IN - ALMOST_FULL_FLAG,
-			pmi_almost_empty_flag => ALMOST_EMPTY_FLAG,
-			pmi_regmode           => "noreg",
-			pmi_resetmode         => "async",
-			pmi_family            => DEVICE_FAMILY,
-			pmi_implementation    => MEMORY)
-		port map(
-			Data        => FifoData_DI,
-			WrClock     => WrClock_CI,
-			RdClock     => RdClock_CI,
-			WrEn        => FifoControl_SI.WriteSide.Write_S,
-			RdEn        => FIFORead_S,
-			Reset       => Reset_RI,
-			RPReset     => Reset_RI,
-			Q           => DataInReg_D,
-			Empty       => FIFOEmpty_S,
-			Full        => FifoControl_SO.WriteSide.Full_S,
-			AlmostEmpty => FIFOAlmostEmpty_S,
-			AlmostFull  => FifoControl_SO.WriteSide.AlmostFull_S);
+	pmiFifoDC : if DEVICE_FAMILY /= "ECP3" generate
+		fifoDualClock : component work.pmi_components.pmi_fifo_dc
+			generic map(
+				pmi_data_width_w      => DATA_WIDTH_IN,
+				pmi_data_width_r      => DATA_WIDTH,
+				pmi_data_depth_w      => DATA_DEPTH_IN,
+				pmi_data_depth_r      => DATA_DEPTH,
+				pmi_full_flag         => DATA_DEPTH_IN,
+				pmi_empty_flag        => 0,
+				pmi_almost_full_flag  => DATA_DEPTH_IN - ALMOST_FULL_FLAG,
+				pmi_almost_empty_flag => ALMOST_EMPTY_FLAG,
+				pmi_regmode           => "noreg",
+				pmi_resetmode         => "async",
+				pmi_family            => DEVICE_FAMILY,
+				pmi_implementation    => MEMORY)
+			port map(
+				Data        => FifoData_DI,
+				WrClock     => WrClock_CI,
+				RdClock     => RdClock_CI,
+				WrEn        => FifoControl_SI.WriteSide.Write_S,
+				RdEn        => FIFORead_S,
+				Reset       => Reset_RI,
+				RPReset     => Reset_RI,
+				Q           => DataInReg_D,
+				Empty       => FIFOEmpty_S,
+				Full        => FifoControl_SO.WriteSide.Full_S,
+				AlmostEmpty => FIFOAlmostEmpty_S,
+				AlmostFull  => FifoControl_SO.WriteSide.AlmostFull_S);
+	end generate pmiFifoDC;
+
+	ipExpressFifoDC : if DEVICE_FAMILY = "ECP3" generate
+		assert (DATA_WIDTH = 32) report "FIFODualClockDouble on ECP3 is hard-coded to 32bit data width." severity FAILURE;
+		assert (DATA_DEPTH = 512) report "FIFODualClockDouble on ECP3 is hard-coded to 512 elements data depth." severity FAILURE;
+		assert (ALMOST_EMPTY_FLAG = 8) report "FIFODualClockDouble on ECP3 is hard-coded to 8 elements for the almost empty flag." severity FAILURE;
+		assert (ALMOST_FULL_FLAG = 2) report "FIFODualClockDouble on ECP3 is hard-coded to 2 elements for the almost full flag." severity FAILURE;
+
+		fifoDualClock : entity work.FIFODualClockDoubleECP3
+			port map(
+				Data        => FifoData_DI,
+				WrClock     => WrClock_CI,
+				RdClock     => RdClock_CI,
+				WrEn        => FifoControl_SI.WriteSide.Write_S,
+				RdEn        => FIFORead_S,
+				Reset       => Reset_RI,
+				RPReset     => Reset_RI,
+				Q           => DataInReg_D,
+				Empty       => FIFOEmpty_S,
+				Full        => FifoControl_SO.WriteSide.Full_S,
+				AlmostEmpty => FIFOAlmostEmpty_S,
+				AlmostFull  => FifoControl_SO.WriteSide.AlmostFull_S);
+	end generate ipExpressFifoDC;
 
 	readSideOutputsRegisteringLogic : process(State_DP, FIFOEmpty_S, FIFOAlmostEmpty_S, FifoControl_SI)
 	begin
