55258c4f66a863b87ed7ea46527d06f9de5e5f0b
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2015-04-17 23:25:28 +0000

    StreamTester: complete rename.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@6473 b7f4320f-462c-0410-a916-d9f35bb82d52

--- devices/logic/StreamTester/MachXO/generate_XSVF.sh
+++ devices/logic/StreamTester/MachXO/generate_XSVF.sh
@@ -2,7 +2,7 @@
 
 echo "Converting SVF file to XSVF format ..."
 
-../../svf2xsvf/svf2xsvf502 -extensions -w -i StreamTester_FX2_MachXO/StreamTester_FX2_MachXO.svf -o StreamTester_FX2_MachXO/StreamTester_FX2_MachXO.xsvf
+../../svf2xsvf/svf2xsvf502 -extensions -w -i StreamTester_MachXO/StreamTester_MachXO.svf -o StreamTester_MachXO/StreamTester_MachXO.xsvf
 
-cp -f StreamTester_FX2_MachXO/StreamTester_FX2_MachXO.xsvf ../bin/StreamTester_FX2_MachXO.xsvf
-cp -f StreamTester_FX2_MachXO/StreamTester_FX2_MachXO_StreamTester_FX2_MachXO.jed ../bin/StreamTester_FX2_MachXO.jed
+cp -f StreamTester_MachXO/StreamTester_MachXO.xsvf ../bin/StreamTester_MachXO.xsvf
+cp -f StreamTester_MachXO/StreamTester_MachXO_StreamTester_MachXO.jed ../bin/StreamTester_MachXO.jed
