94fa4d5a9961f364463f9cd2d56f4d749625b923
llongi <llongi@b7f4320f-462c-0410-a916-d9f35bb82d52>
2015-07-15 18:02:45 +0000

    jAER: add experimental stub for TNTell.
    git-svn-id: svn://svn.code.sf.net/p/jaer/code@6959 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ jAER/trunk/src/org/ine/telluride/jaer/tell2015/tnpedtracker/ClusterExtractorTN.java
@@ -0,0 +1,11 @@
+package org.ine.telluride.jaer.tell2015.tnpedtracker;
+
+import net.sf.jaer.chip.AEChip;
+import net.sf.jaer.eventprocessing.tracking.RectangularClusterTracker;
+
+public class ClusterExtractorTN extends RectangularClusterTracker {
+	public ClusterExtractorTN(AEChip chip) {
+		super(chip);
+		// TODO Auto-generated constructor stub
+	}
+}
